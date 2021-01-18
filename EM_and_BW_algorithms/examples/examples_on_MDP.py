8e2461298b6507e55e6d0ee060529fbcae0785b1
Rapfff <23459708+Rapfff@users.noreply.github.com>
2021-01-18 19:53:46 +0000

    Add files via upload

--- EM_and_BW_algorithms/examples/examples_on_MDP.py
+++ EM_and_BW_algorithms/examples/examples_on_MDP.py
@@ -4,7 +4,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 from Estimation_algorithms_MDP import *
-from examples_models import modelMDP1
+from examples_models import modelMDP2, scheduler_MDP2_random
 from MDP import *
 
 def fullyobservable_sequences(mdp, scheduler, length_sequences, nb_sequences):
@@ -24,3 +24,24 @@ def fullyobservable_blackbox(mdp, length_sequences, nb_sequences):
 	print("Learned MDP:")
 	Estimation_algorithm_fullyobservable_MDP().learnFromBlackBox(mdp, nb_sequences,length_sequences).pprint()
 
+
+
+m = modelMDP2()
+s = scheduler_MDP2_random()
+
+training_set_seq = []
+training_set_val = []
+
+for i in range(1000):
+	trace = m.run(10, s)
+
+	if not trace in training_set_seq:
+		training_set_seq.append(res)
+		training_set_val.append(0)
+
+	training_set_val[training_set_seq.index(trace)] += 1
+
+training_set = [training_set_seq,training_set_val]
+
+algo = Estimation_algorithms_MDP(??,['tl','t','tr','l','n','r','bl','b','br'],['t','l','b','r'])
+algo.problem3(training_set)
