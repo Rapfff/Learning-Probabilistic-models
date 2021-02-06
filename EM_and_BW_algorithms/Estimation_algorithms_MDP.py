09b16b859452a15c8dae7c8dc9fcb7d80b9bc8d9
Rapfff <23459708+Rapfff@users.noreply.github.com>
2021-02-06 17:03:54 +0000

    Add files via upload

--- EM_and_BW_algorithms/Estimation_algorithms_MDP.py
+++ EM_and_BW_algorithms/Estimation_algorithms_MDP.py
@@ -61,8 +61,7 @@ class Estimation_algorithm_fullyobservable_MDP:
 		return self.learnFromSequences(sequences)
 
 
-
-class Estimation_algorithm_MDP_sequences:
+class Estimation_algorithm_MDP:
 	def __init__(self,h,alphabet,actions):
 		"""
 		h is a MDP
@@ -88,7 +87,7 @@ class Estimation_algorithm_MDP_sequences:
 			print(c)
 			return False
 		
-		currentloglikelihood = self.hhat.logLikelihoodTraces(self.traces)
+		currentloglikelihood = self.logLikelihood()
 		print(c,"- loglikelihood :",currentloglikelihood)
 		if self.prevloglikelihood == currentloglikelihood:
 			self.prevloglikelihood = currentloglikelihood
@@ -98,31 +97,49 @@ class Estimation_algorithm_MDP_sequences:
 			return False
 
 	def h_g(self,s1,act,s2,obs):
-		return self.h.states[s1].g(act,s2,obs)
+		return self.h.g(s1,act,s2,obs)
 
 	def hhat_g(self,s1,act,s2,obs):
-		return self.hhat.states[s1].g(act,s2,obs)
+		return self.hhat.g(s1,act,s2,obs)
+	
+	def precomputeMatrices(self,sequence):
+		"""Here we compute all the values alpha(k,t) and beta(t,k) for a given sequence"""
+		self.alpha_matrix = []
 
-	def problem3(self,traces):
-		"""
-		Given, for several finite-memory scheduler, a set of sequences of observations,
-		it adapts the parameters of h in order to maximize the probability to get 
-		these sequences of observations.
-		data = [[schedulerA, [sequenceA1,sequenceA2,...]],[schedulerB, [sequenceB1,sequenceB2,...]],...]
-		sequence = [ob1,obs2,...]
-		traces = [[trace1,trace2,...],[number_of_trace1,number_of_trace2,...]]
-		trace = [action,obs1,action2,obs2,...,actionx,obsx]
-		"""
+		for s in range(len(self.h.states)):
+			if s == self.h.initial_state:
+				self.alpha_matrix.append([1.0])
+			else:
+				self.alpha_matrix.append([0.0])
+		
+		for k in range(len(sequence)):
+			for s in range(len(self.h.states)):
+				summ = 0
+				for ss in range(len(self.h.states)):
+					for action, proba_action in self.getAction(k):
+						p = self.h_g(ss,action,s,sequence[k])*proba_action
+						if p > 0:
+							summ += self.alpha_matrix[ss][k]*p
+				self.alpha_matrix[s].append(summ)
+
+		self.beta_matrix = []
+
+		for s in range(len(self.h.states)):
+			self.beta_matrix.append([1.0])
+		
+		for k in range(len(sequence)-1,-1,-1):
+			for s in range(len(self.h.states)):
+				summ = 0
+				for ss in range(len(self.h.states)):
+					for action, proba_action in self.getAction(k):
+						p = self.h_g(s,action,ss,sequence[k])*proba_action
+						if p > 0:
+							summ += self.beta_matrix[ss][1 if ss<s else 0]*p
+				self.beta_matrix[s].insert(0,summ)
+
+	def problem3(self,observations):
 		start_time = time()
-		self.traces = traces
 		counter = 0
-
-		observations = []
-		for seq in traces[0]:
-			for i in range(1,len(seq),2):
-				if not seq[i] in observations:
-					observations.append(seq[i])
-
 		while True:
 			new_states = []
 			for i in range(len(self.h.states)):
@@ -160,39 +177,6 @@ class Estimation_algorithm_MDP_sequences:
 		self.h.pprint()
 		return [self.prevloglikelihood,time()-start_time]
 
-	
-	def precomputeMatrices(self,sequence,actions):
-		"""Here we compute all the values alpha(k,t) and beta(t,k) for a given sequence"""
-		self.alpha_matrix = []
-
-		for s in range(len(self.h.states)):
-			if s == self.h.initial_state:
-				self.alpha_matrix.append([1.0])
-			else:
-				self.alpha_matrix.append([0.0])
-		
-		for k in range(len(sequence)):
-			for s in range(len(self.h.states)):
-				summ = 0
-				for ss in range(len(self.h.states)):
-					p = self.h_g(ss,actions[k],s,sequence[k])
-					summ += self.alpha_matrix[ss][k]*p
-				self.alpha_matrix[s].append(summ)
-
-		self.beta_matrix = []
-
-		for s in range(len(self.h.states)):
-			self.beta_matrix.append([1.0])
-		
-		for k in range(len(sequence)-1,-1,-1):
-			for s in range(len(self.h.states)):
-				summ = 0
-				for ss in range(len(self.h.states)):
-					p = self.h_g(s,actions[k],ss,sequence[k])
-					if p > 0:
-						summ += self.beta_matrix[ss][1 if ss<s else 0]*p
-				self.beta_matrix[s].insert(0,summ)
-
 	def gamma(self,s,k):
 		"""
 		Returns the probability to generate the kth obs of o in state s * bigK
@@ -200,18 +184,43 @@ class Estimation_algorithm_MDP_sequences:
 		"""
 		return self.alpha_matrix[s][k]*self.beta_matrix[s][k]/self.bigK
 
-	def xi(self,action,s1,s2,k):
+	def xi(self,sequence,action,s1,s2,k):
 		"""
-		Returns the probabilty to move from state s1 to state s2 at time step k
+		Returns the probabilty to move from state s1 to state s2 with action at time step k
 		Note: it's important to compute self.bigK and the TUKmatrix before
 		"""
-		return self.alpha_matrix[s1][k]*self.h_g(s1,action,s2,self.sequence[k])*self.beta_matrix[s2][k+1]/self.bigK
+		return self.alpha_matrix[s1][k]*self.h_g(s1,action,s2,sequence[k])*self.beta_matrix[s2][k+1]/self.bigK
 
 	def computeK(self):
 		"""
 		Returns sum over ss of alpha(k-1,ss) * beta(ss,k)
 		"""
 		self.bigK = self.beta_matrix[self.h.initial_state][0]
+
+class Estimation_algorithm_MDP_sequences(Estimation_algorithm_MDP):
+	def problem3(self,traces):
+		"""
+		Given a set of sequences of pairs action-observation,
+		it adapts the parameters of h in order to maximize the probability to get 
+		these sequences of observations.
+		traces = [[trace1,trace2,...],[number_of_trace1,number_of_trace2,...]]
+		trace = [action,obs1,action2,obs2,...,actionx,obsx]
+		"""
+		self.traces = traces
+		observations = []
+		for seq in traces[0]:
+			for i in range(1,len(seq),2):
+				if not seq[i] in observations:
+					observations.append(seq[i])
+
+		return super().problem3(observations)
+
+	def logLikelihood(self):
+		return self.hhat.logLikelihoodTraces(self.traces)
+
+	def getAction(self,k):
+		"""Return the action at time step k, and probaility=1.0"""
+		return [(self.sequence_actions[k],1.0)]
 
 	def ghatmultiple(self,s1,s2,obs):
 		"""
@@ -225,16 +234,77 @@ class Estimation_algorithm_MDP_sequences:
 			den.append(0.0)
 
 		for seq in range(len(self.traces[0])):
-			actions = [self.traces[0][seq][i] for i in range(0,len(self.traces[0][seq]),2)]
-			self.sequence = [self.traces[0][seq][i+1] for i in range(0,len(self.traces[0][seq]),2)]
+			self.sequence_actions = [self.traces[0][seq][i] for i in range(0,len(self.traces[0][seq]),2)]
+			sequence_obs = [self.traces[0][seq][i+1] for i in range(0,len(self.traces[0][seq]),2)]
 			times = self.traces[1][seq]
 			
-			self.precomputeMatrices(self.sequence,actions)
+			self.precomputeMatrices(sequence_obs)
 			self.computeK()
 			
-			for k in range(len(self.sequence)):
-				den[self.actions.index(actions[k])] += self.gamma(s1,k) * times
-				if self.sequence[k] == obs:
-					num[self.actions.index(actions[k])] += self.xi(actions[k],s1,s2,k) * times
+			for k in range(len(sequence_obs)):
+				den[self.actions.index(self.sequence_actions[k])] += self.gamma(s1,k) * times
+				if sequence_obs[k] == obs:
+					num[self.actions.index(self.sequence_actions[k])] += self.xi(sequence_obs,self.sequence_actions[k],s1,s2,k) * times
+		
+		return [num[i]/den[i] if den[i] != 0.0 else 0.0 for i in range(len(num))]
+
+
+
+class Estimation_algorithm_MDP_schedulers(Estimation_algorithm_MDP):
+	def problem3(self,data):
+		"""
+		Given a set of pairs scheduler-sequence of observations,
+		it adapts the parameters of h in order to maximize the probability to get 
+		these sequences of observations.
+		data = [(scheduler1,traces1),(scheduler2,traces2)]
+		traces = [[trace1,trace2,...],[number_of_trace1,number_of_trace2,...]]
+		trace = [obs1,obs2,...]
+		"""
+		self.data = data
+		observations = []
+		for sched in data:
+			for seq in sched[1]:
+				for i in seq:
+					if not i in observations:
+						observations.append(i)
+
+		return super().problem3(observations)
+	
+	def logLikelihood(self):
+		return self.hhat.logLikelihoodObservationsScheduler(self.data)
+
+	def getAction(self,k):
+		"""Return the action at time step k, and probaility=1.0"""
+		return self.scheduler.get_actions(self.seq_sched_state[k])
+
+	def ghatmultiple(self,s1,s2,obs):
+		"""
+		returns the new values, for all the possible actions, for g[action][s1][s2][obs], format:
+		[p1,p2,p3,...] with p1 = g[action1][s1][s2][obs], p2 = g[action2][s1][s2][obs], ...
+		"""
+		num = []
+		den = []
+		for i in range(len(self.actions)):
+			num.append(0.0)
+			den.append(0.0)
+
+		
+		for data_index in range(len(self.data)):
+			self.scheduler = self.data[data_index][0]
+			traces = self.data[data_index][1]
+
+			for seq in range(len(traces[0])):
+				sequence = traces[0][seq]
+				times = traces[1][seq]
+				self.seq_sched_state = self.scheduler.get_sequence_states(sequence)
+				
+				self.precomputeMatrices(sequence)
+				self.computeK()
+				
+				for k in range(len(sequence)):
+					for act in range(len(self.actions)):
+						den[act] += self.gamma(s1,k) * times
+						if sequence[k] == obs:
+							num[act] += self.xi(sequence,self.actions[act],s1,s2,k) * times * self.scheduler.get_probability(act,self.seq_sched_state[k])
 		
 		return [num[i]/den[i] if den[i] != 0.0 else 0.0 for i in range(len(num))]
--- EM_and_BW_algorithms/MDP.py
+++ EM_and_BW_algorithms/MDP.py
@@ -2,31 +2,54 @@ from tools import resolveRandom
 from math import log
 
 class FiniteMemoryScheduler:
-	def __init__(self,next_matrix,transition_matrix):
+	def __init__(self,action_matrix,transition_matrix):
 		"""
-		next_matrix = {scheduler_state: [[proba1,proba2,...],[action1,action2,...]],
-					   scheduler_state: [[proba1,proba2,...],[action1,action2,...]],
-					   ...}
+		action_matrix = {scheduler_state: [[proba1,proba2,...],[action1,action2,...]],
+					     scheduler_state: [[proba1,proba2,...],[action1,action2,...]],
+					     ...}
 		transition_matrix = {obs1: [scheduler_state_dest_if_current_state_=_0,scheduler_state_dest_if_current_state_=_1,...]
 							 obs2: [scheduler_state_dest_if_current_state_=_0,scheduler_state_dest_if_current_state_=_1,...]
 							 ...}
 		"""	
 		self.s = 0
-		self.next_matrix = next_matrix
+		self.action_matrix = action_matrix
 		self.transition_matrix = transition_matrix
+
+	def reset(self):
+		self.s = 0
 
 	def get_action(self):
 		"""return an action to execute by the agent"""
-		return self.next_matrix[self.s][1][resolveRandom(self.next_matrix[self.s][0])]
+		return self.action_matrix[self.s][1][resolveRandom(self.action_matrix[self.s][0])]
 
 	def add_observation(self,obs):
 		"""give to the scheduler the new observation seen by the agent"""
 		if obs in self.transition_matrix:
 			self.s = self.transition_matrix[obs][self.s]
 
-	def get_actions(self):
+	def get_actions(self,s=None):
 		"""return the actions (and their probability) that the agent can execute now"""
-		return self.next_matrix[self.s]
+		if s==None:
+			return self.action_matrix[self.s]
+		else:
+			return self.action_matrix[s]
+
+	def get_sequence_states(self,seq_obs):
+		"""given a sequence of observations, return the sequence of states the scheduler goes through"""
+		self.reset()
+		res = [0]
+		for o in seq_obs:
+			self.add_observation(o)
+			res.append(self.s)
+		return res
+
+	def get_probability(self,action,state=None):
+		"""given a scheduler state and an action return the probability to execute this action in this state"""
+		if state == None:
+			state = self.s
+		if not action in self.transition_matrix[state][1]:
+			return 0
+		return self.action_matrix[state][0][self.transition_matrix[state][1].index(action)]
 
 
 class MDP_state:
@@ -72,12 +95,16 @@ class MDP:
 			return 1.0
 		else:
 			return 0.0
+
+	def g(self,s1,action,s2,obs):
+		return self.states[s1].g(action,s2,obs)
 			
-	def run(self,number_steps,scheduler):
+	def run(self,number_steps,scheduler,with_action=True):
 		#output = [self.states[self.initial_state].observation]
 		res = []
 		#actions = []
 		current = self.initial_state
+		scheduler.reset()
 
 		while len(res)/2 < number_steps:
 			action = scheduler.get_action()
@@ -85,7 +112,8 @@ class MDP:
 			while action not in self.states[current].next_matrix:
 				action = scheduler.get_action()
 
-			res.append(action)
+			if with_action:
+				res.append(action)
 			next_state, observation = self.states[current].next(action)
 
 			#output.append(observation)
@@ -105,51 +133,35 @@ class MDP:
 					if self.states[i].next_matrix[action][0][j] > 0.0:
 						print("s",i," - (",action,") -> s",self.states[i].next_matrix[action][1][j]," : ",self.states[i].next_matrix[action][2][j],' : ',self.states[i].next_matrix[action][0][j],sep='')
 		print()
-	
-	def allStatesPathIterative(self, start, trace):
-		"""return all the states path from start that can generate trace"""
-		res = []
-		action = trace[0]
-		obs  = trace[1]
 
-		if not action in self.states[start].actions():
-			return []
-
-		for i in range(len(self.states[start].next_matrix[action][1])):
-
-			if self.states[start].next_matrix[action][0][i] > 0 and self.states[start].next_matrix[action][2][i] == obs:
-				if len(trace) == 2:
-					res.append([start,self.states[start].next_matrix[action][1][i]])
-				else:
-					t = self.allStatesPathIterative(self.states[start].next_matrix[action][1][i],trace[2:])
-					for j in t:
-						res.append([start]+j)
-		return res
-	
+	#-------------------------------------------
 	def allStatesPathTrace(self,trace):
-		"""return all the states path that can generate obs_seq"""
-		res = []
-		for j in self.allStatesPathIterative(self.initial_state,trace):
-			res.append(j)
+		res = [[0]]
+		c = 0
+		while c < len(trace):
+			new = []
+			for p in res:
+				for s in range(len(self.states)):
+					if self.g(p[-1],trace[c], s, trace[c+1]) > 0.0:
+						new.append(p+[trace[c],trace[c+1],s])
+			c += 2
+			res = new
 		return res
 
-	def probabilityStateTrace(self,states_path, trace):
-		"""return the probability to get this states_path generating this observations sequence"""
-		if states_path[0] != self.initial_state:
-			return 0.0
-		else:
-			res = 1.0
-		for i in range(len(states_path)-1):
-			if res == 0.0:
-				return 0.0
-			res *= self.states[states_path[i]].g(trace[i*2],states_path[i+1],trace[i*2+1])
-		return res
+	def probabilityStateActionObservation(self,path):
+		c = 0
+		p = 1
+		while p > 0.0 and c < len(path)-1:
+			p *= self.g(path[c],path[c+1],path[c+3],path[c+2])
+			c += 3
+		return p
+
 
 	def probabilityTrace(self,trace):
 		"""return the probability to get this trace"""
 		res = 0
 		for p in self.allStatesPathTrace(trace):
-			res += self.probabilityStateTrace(p,trace)
+			res += self.probabilityStateActionObservation(p)
 		return res
 
 	def logLikelihoodTraces(self,sequences):
@@ -157,7 +169,51 @@ class MDP:
 		for i in range(len(sequences[0])):
 			p = self.probabilityTrace(sequences[0][i])
 			if p == 0:
-				print(sequences[0][i])
 				return -256
 			res += log(p) * sequences[1][i]
 		return res / sum(sequences[1])
+
+	#-------------------------------------------
+	def allStatesPathObservations(self,seq_obs):
+		res = [[0]]
+		c = 0
+		while c < len(seq_obs):
+			new = []
+			for p in res:
+				for act in self.states[p[-1]].actions():
+					for s in range(len(self.states)):
+						if self.g(p[-1],act, s, seq_obs[c]) > 0.0:
+							new.append(p+[act,seq_obs[c],s])
+			c += 1
+			res = new
+		return res
+
+	def probabilityStateActionObservationWithScheduler(self,path,scheduler):
+		scheduler.reset()
+		c = 0
+		p = 1
+		while p > 0.0 and c < len(path):
+			p *= scheduler.get_probability(path[c+1])
+			p *= self.g(path[c],path[c+1],path[c+3],path[c+2])
+			scheduler.add_observation(path[c+2])
+			c += 3
+		return p
+
+	def probabilityObservationsScheduler(self,seq_obs,scheduler):
+		"""return the probability to get this sequence of observations with this scheduler"""
+		res = 0
+		for p in self.allStatesPathObservations(seq_obs):
+			res += self.probabilityStateActionObservationWithScheduler(p,scheduler)
+		return res
+
+	def logLikelihoodObservationsScheduler(self,data):
+		res = 0
+		nb_seq = 0
+		for scheduler,sequences in data:
+			nb_seq += sum(sequences[1])
+			for i in range(len(sequences[0])):
+				p = self.probabilityObservationsScheduler(sequences[0][i],scheduler)
+				if p == 0:
+					return -256
+				res += log(p) * sequences[1][i]
+		return res / nb_seq
