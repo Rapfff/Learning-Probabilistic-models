bd6031e7ccc2b3ef3802249a8c4b82ceb5ce1dd6
Rapfff <23459708+Rapfff@users.noreply.github.com>
2021-04-30 11:20:16 +0000

    Add files via upload

--- EM_and_BW_algorithms/active_vs_passive/active_vs_passive.py
+++ EM_and_BW_algorithms/active_vs_passive/active_vs_passive.py
@@ -6,18 +6,10 @@ from examples.examples_models import modelMDP_random, scheduler_uniform
 from examples.small_gridMDP import SmallGrid
 from Estimation_algorithms_MDP_multithreading import Estimation_algorithm_MDP
 from datetime import datetime
-from tools import randomProbabilities, generateSet
+from tools import randomProbabilities, generateSet, mergeSets
 from Active_Learning_MDP import Active_Learning_MDP
 from MDP import loadMDP
 
-def mergeSets(s1,s2):
-	for seq in s2[0]:
-		if seq in s1[0]:
-			s1[1][s1[0].index(seq)] += 1
-		else:
-			s1[0].append(seq)
-			s1[1].append(1)
-	return s1
 
 nb_states = 9
 original = SmallGrid()
@@ -25,14 +17,14 @@ observations = original.observations()
 actions = original.actions()
 
 set_size_learning = 100
-set_size_test = 500
+set_size_test = 100
 sequence_size = 10
 
 s = scheduler_uniform(actions)
-"""
-training_set = generateSet(m,set_size_learning,sequence_size,s,True)
-test_set = generateSet(m,set_size_learning,sequence_size,s,True)
 
+#training_set = generateSet(m,set_size_learning,sequence_size,s,True)
+#test_set = generateSet(m,set_size_learning,sequence_size,s,True)
+"""
 f = open("training_set.txt",'w')
 for i in range(len(training_set[0])):
 	f.write(str(training_set[0][i])+'\n')
@@ -45,8 +37,8 @@ for i in range(len(test_set[0])):
 f.close()
 m = modelMDP_random(nb_states,observations,actions)
 m.save("initial_model.txt")
-"""
 
+"""
 training_set = [[],[]]
 
 f = open("training_set.txt",'r')
@@ -65,9 +57,13 @@ f.close()
 m = loadMDP("initial_model.txt")
 print(datetime.now(),"Active Learning:")
 algo = Active_Learning_MDP(m,observations,actions)
-algo.learn(training_set,0.1,0.25,20,50,limit=0.01)
+algo.learn(training_set,0.1,0.25,10,30,limit=0.01)
 history = algo.history
+f = open("history.txt",'w')
+f.write(str(history))
+f.close()
 # -----------------------------------------------
+
 m = loadMDP("initial_model.txt")
 print(datetime.now(),"Passive Learning:")
 algo = Estimation_algorithms_MDP_multithreading(m,observations,actions)
@@ -78,8 +74,10 @@ for i in range(len(history)):
 	t = generateSet(original,history[i],sequence_size,s,True)
 	algo.problem3(mergeSets(training_set,t),"passive_models/passive_models_"+str(i+1)+".txt",0.01,str(i+1)+'/'+str(len(history)+1))
 
-"""
+
+
 import matplotlib.pyplot as plt
+
 test_set = [[],[]]
 f = open("test_set.txt",'r')
 l = f.readline()
@@ -94,21 +92,29 @@ f.close()
 
 results_active = []
 results_passive= []
-for c in range(len(history)+1):
+for c in range(50):
 	m = loadMDP("active_models/active_models_"+str(c)+".txt")
+	m.pprint()
+	input()
 	results_active.append(m.logLikelihoodTraces(test_set))
 	m = loadMDP("passive_models/passive_models_"+str(c)+".txt")
 	results_passive.append(m.logLikelihoodTraces(test_set))
 
-x_val = [set_size_learning+sum(history[:i+1]) for i in range(len(history))]
-x_val.insert(0,set_size_learning)
+print(results_passive)
+print()
+print(results_active)
 
-x = [i for i in range(len(x_val))]
+#x_val = [set_size_learning+sum(history[:i+1]) for i in range(len(history))]
+#x_val.insert(0,set_size_learning)
+x_val = [i for i in range(50)]
+x = [i for i in range(50)]
+
 fig, ax = plt.subplots()
+width = 0.35  # the width of the bars
+
 rects1 = ax.bar([i - width/2 for i in x], results_active, width, label='Active Learning')
 rects2 = ax.bar([i + width/2 for i in x], results_passive, width, label='Passive Learning')
 
-width = 0.35  # the width of the bars
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('lolikelihood of the test set')
 ax.set_ylabel('size of the test set')
