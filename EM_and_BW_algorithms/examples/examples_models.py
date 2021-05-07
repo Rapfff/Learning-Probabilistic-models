ea70fa8668a1feb9bad9f43ff87410d8e29801b0
Rapfff <23459708+Rapfff@users.noreply.github.com>
2021-05-07 16:27:50 +0000

    Add files via upload

---
+++ EM_and_BW_algorithms/examples/examples_models.py
@@ -0,0 +1,315 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from src.models.HMM import *
+from src.models.MCGT import *
+from src.models.MCGS import *
+from src.models.MDP import *
+from src.tools import randomProbabilities
+
+# ---- HMM ----------------------------
+
+def modelHMM1():
+	h_s0 = HMM_state([[1.0],['$']],[[0.3,0.4,0.3],[1,2,3]])
+	h_s1 = HMM_state([[1.0],['a']],[[1.0],[4]])
+	h_s2 = HMM_state([[1.0],['b']],[[1.0],[4]])
+	h_s3 = HMM_state([[1.0],['c']],[[1.0],[4]])
+	h_s4 = HMM_state([[1.0],['d']],[[1.0],[4]])
+	return HMM([h_s0,h_s1,h_s2,h_s3,h_s4],0,"HMM1")
+
+
+def modelHMM1_equiprobable():
+	h_s0 = HMM_state([[0.2,0.2,0.2,0.2,0.2],['$','a','b','c','d']],[[0.2,0.2,0.2,0.2,0.2],[0,1,2,3,4]])
+	h_s1 = HMM_state([[0.2,0.2,0.2,0.2,0.2],['$','a','b','c','d']],[[0.2,0.2,0.2,0.2,0.2],[0,1,2,3,4]])
+	h_s2 = HMM_state([[0.2,0.2,0.2,0.2,0.2],['$','a','b','c','d']],[[0.2,0.2,0.2,0.2,0.2],[0,1,2,3,4]])
+	h_s3 = HMM_state([[0.2,0.2,0.2,0.2,0.2],['$','a','b','c','d']],[[0.2,0.2,0.2,0.2,0.2],[0,1,2,3,4]])
+	h_s4 = HMM_state([[0.2,0.2,0.2,0.2,0.2],['$','a','b','c','d']],[[0.2,0.2,0.2,0.2,0.2],[0,1,2,3,4]])
+	return HMM([h_s0,h_s1,h_s2,h_s3,h_s4],0,"HMM1_equiprobable")
+
+def modelHMM2():
+	h_s0 = HMM_state([[0.5,0.5],['x','y']],[[0.3,0.7,0.0],[1,2,3]])
+	h_s1 = HMM_state([[1.0],['a']],[[1.0],[3]])
+	h_s2 = HMM_state([[1.0],['b']],[[1.0],[3]])
+	h_s4 = HMM_state([[1.0],['d']],[[1.0],[3]])
+	return HMM([h_s0,h_s1,h_s2,h_s4],0,"HMM2")
+
+def modelHMM2_random():
+	h_s0 = HMM_state([randomProbabilities(5),['x','y','a','b','d']],[randomProbabilities(4),[0,1,2,3]])
+	h_s1 = HMM_state([randomProbabilities(5),['x','y','a','b','d']],[randomProbabilities(4),[0,1,2,3]])
+	h_s2 = HMM_state([randomProbabilities(5),['x','y','a','b','d']],[randomProbabilities(4),[0,1,2,3]])
+	h_s3 = HMM_state([randomProbabilities(5),['x','y','a','b','d']],[randomProbabilities(4),[0,1,2,3]])
+	return HMM([h_s0,h_s1,h_s2,h_s3],0,"HMM2_random")
+
+def modelHMM2_equiprobable():
+	h_s0 = HMM_state([[0.25,0.25,0.25,0.25,0.0],['x','y','a','b','d']],[[0.25,0.25,0.25,0.25],[0,1,2,3]])
+	h_s1 = HMM_state([[0.25,0.25,0.25,0.25,0.0],['x','y','a','b','d']],[[0.25,0.25,0.25,0.25],[0,1,2,3]])
+	h_s2 = HMM_state([[0.25,0.25,0.25,0.25,0.0],['x','y','a','b','d']],[[0.25,0.25,0.25,0.25],[0,1,2,3]])
+	h_s3 = HMM_state([[0.0,0.0,0.0,0.0,1.0],['x','y','a','b','d']],[[0.25,0.25,0.25,0.25],[0,1,2,3]])
+	return HMM([h_s0,h_s1,h_s2,h_s3],0,"HMM2_equiprobable")
+
+
+def modelHMM3():
+	h_s0 = HMM_state([[1.0],['$']],[[0.5,0.5],[1,2]])
+	h_s1 = HMM_state([[0.8,0.2],['a','b']],[[1.0],[0]])
+	h_s2 = HMM_state([[0.2,0.8],['a','b']],[[1.0],[0]])
+	return HMM([h_s0,h_s1,h_s2],0,"HMM3")
+
+def modelHMM4():
+	h_s0 = HMM_state([[0.4,0.6],['x','y']],[[0.5,0.5],[1,2]])
+	h_s1 = HMM_state([[0.8,0.2],['a','b']],[[1.0],[3]])
+	h_s2 = HMM_state([[0.1,0.9],['a','b']],[[1.0],[4]])
+	h_s3 = HMM_state([[0.5,0.5],['x','y']],[[0.8,0.1,0.1],[0,1,2]])
+	h_s4 = HMM_state([[1.0],['y']],[[1.0],[3]])
+	return HMM([h_s0,h_s1,h_s2,h_s3,h_s4],0,"HMM4")
+
+def modelHMM_random(number_states, alphabet):
+	states = []
+	for s in range(number_states):
+		states.append(HMM_state([randomProbabilities(len(alphabet)),alphabet],[randomProbabilities(number_states),list(range(number_states))]))
+	return HMM(states,0)
+
+# ---- MCGT ---------------------------
+
+def modelMCGT1():
+	g_s0 = MCGT_state([[0.15,0.15,0.2,0.2,0.15,0.15],[1,1,2,2,3,3],['x','y','x','y','x','y']])
+	g_s1 = MCGT_state([[1.0],[4],['a']])
+	g_s2 = MCGT_state([[1.0],[4],['b']])
+	g_s3 = MCGT_state([[1.0],[4],['c']])
+	g_s4 = MCGT_state([[0.5,0.5],[4,4],['d','e']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3,g_s4],0,"MCGT1")
+
+def modelMCGT2():
+	g_s0 = MCGT_state([[0.2,0.2,0.3,0.3],[1,1,2,2],['x','y','x','y']])
+	g_s1 = MCGT_state([[1.0],[3],['a']])
+	g_s2 = MCGT_state([[1.0],[3],['b']])
+	g_s3 = MCGT_state([[1.0],[3],['d']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3],0,"MCGT2")
+
+def modelMCGT3():
+	g_s0 = MCGT_state([[1/3,1/3,1/3],[1,2,3],['$','$','$']])
+	g_s1 = MCGT_state([[1.0],[4],['a']])
+	g_s2 = MCGT_state([[1.0],[4],['b']])
+	g_s3 = MCGT_state([[1.0],[4],['c']])
+	g_s4 = MCGT_state([[1.0],[4],['d']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3,g_s4],0,"MCGT3")
+
+def modelMCGT4():
+	g_s0 = MCGT_state([[0.5,0.5],[1,2],['x','y']])
+	g_s1 = MCGT_state([[0.4,0.1,0.35,0.15],[3,3,4,4],['a','b','a','b']])
+	g_s2 = MCGT_state([[0.3,0.2,0.1,0.4],[1,1,4,4],['b','a','a','b']])
+	g_s3 = MCGT_state([[0.5,0.5],[4,5],['c','c']])
+	g_s4 = MCGT_state([[1.0],[5],['d']])
+	g_s5 = MCGT_state([[1.0],[5],['e']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3,g_s4,g_s5],0,"MCGT4")
+
+def modelMCGT_game():
+	s_dice = MCGT_state([[1/3,1/3,1/3],[0,0,1],['1-2','3-4','5-6']])
+	s_cards= MCGT_state([[10/13,3/13],[1,2],["Number","Face"]])
+	s_win  = MCGT_state([[1.0],[2],["Win"]])
+	return MCGT([s_dice,s_cards,s_win],0,"MCGT_games")
+
+def modelMCGT_REBER():
+	g_s0 = MCGT_state([[1.0],[1],['B']])
+	g_s1 = MCGT_state([[0.5,0.5],[2,3],['T','P']])
+	g_s2 = MCGT_state([[0.6,0.4],[2,4],['S','X']])
+	g_s3 = MCGT_state([[0.7,0.3],[3,5],['T','V']])
+	g_s4 = MCGT_state([[0.5,0.5],[3,6],['X','S']])
+	g_s5 = MCGT_state([[0.5,0.5],[4,6],['P','V']])
+	g_s6 = MCGT_state([[1.0],[6],['E']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3,g_s4,g_s5,g_s6],0,"MCGT_REBER")
+
+
+def modelMCGT1_assist():
+	g_s0 = MCGT_state([randomProbabilities(6),[1,1,2,2,3,3],['x','y','x','y','x','y']])
+	g_s1 = MCGT_state([randomProbabilities(4),[1,2,3,4],['a','a','a','a']])
+	g_s2 = MCGT_state([randomProbabilities(4),[1,2,3,4],['b','b','b','b']])
+	g_s3 = MCGT_state([randomProbabilities(4),[1,2,3,4],['c','c','c','c']])
+	g_s4 = MCGT_state([randomProbabilities(4),[1,2,3,4],['d','d','d','d']])
+	return MCGT([g_s0,g_s1,g_s2,g_s3,g_s4],0,"MCGT1_assist")
+
+def modelMCGT_equiprobable(nb_states,alphabet):
+	s = []
+	for i in range(nb_states):
+		s += [i] * len(alphabet)
+	obs = alphabet*nb_states
+	
+	states = []
+	for i in range(nb_states):
+		states.append(MCGT_state([[1/len(obs)]*len(obs),s,obs]))
+	return MCGT(states,0,"MCGT_equiprobable_"+str(nb_states)+"states")
+
+def modelMCGT_random(nb_states,alphabet):
+	s = []
+	for i in range(nb_states):
+		s += [i] * len(alphabet)
+	obs = alphabet*nb_states
+	
+	states = []
+	for i in range(nb_states):
+		states.append(MCGT_state([randomProbabilities(len(obs)),s,obs]))
+	return MCGT(states,0,"MCGT_random_"+str(nb_states)+"states")
+
+def modelMCGT1_equiprobable():
+	return modelMCGT_equiprobable(5,['x','y','a','b','c','d'])
+
+
+# ---- MCGS ---------------------------
+
+def modelMCGS1():
+	m_s0  = MCGS_state('',[[0.5,0.5],[5,6]])
+	m_s0x = MCGS_state('x',[[0.3,0.4,0.3],[1,2,3]])
+	m_s0y = MCGS_state('y',[[0.3,0.4,0.3],[1,2,3]])
+	m_s1  = MCGS_state('',[[1.0],[7]])
+	m_s1a = MCGS_state('a',[[1.0],[4]])
+	m_s2  = MCGS_state('',[[1.0],[8]])
+	m_s2b = MCGS_state('b',[[1.0],[4]])
+	m_s3  = MCGS_state('',[[1.0],[9]])
+	m_s3c = MCGS_state('c',[[1.0],[4]])
+	m_s4  = MCGS_state('',[[1.0],[10]])
+	m_s4d = MCGS_state('d',[[1.0],[4]])
+	return MCGS([m_s0,m_s1,m_s2,m_s3,m_s4,m_s0x,m_s0y,m_s1a,m_s2b,m_s3c,m_s4d],[[1.0],[0]])
+
+
+# ---- MDP ----------------------------
+
+def modelMDP_random(nb_states,alphabet,actions):
+	s = []
+	for i in range(nb_states):
+		s += [i] * len(alphabet)
+	obs = alphabet*nb_states
+	
+	states = []
+	for i in range(nb_states):
+		dic = {}
+		for act in actions:
+			dic[act] = [randomProbabilities(len(obs)),s,obs]
+		states.append(MDP_state(dic))
+	return MDP(states,0,"MDP_random_"+str(nb_states)+"states")
+
+def modelMDP1_fullyobservable():
+	m_s0 = MDP_state({'a': [[1.0],[1],['1']], 'b': [[1.0],[2],['2']]})
+	m_s1 = MDP_state({'a': [[0.8,0.2],[2,3],['2','3']], 'b': [[0.9,0.1],[3,4],['3','4']]})
+	m_s2 = MDP_state({'a': [[0.9,0.1],[6,4],['6','4']], 'b': [[1.0],[2],['2']]})
+	m_s3 = MDP_state({'a': [[0.9,0.1],[1,2],['1','2']], 'b': [[1.0],[1],['1']]})
+	m_s4 = MDP_state({'a': [[1.0],[5],['5']], 'b': [[1.0],[2],['4']]})
+	m_s5 = MDP_state({'a': [[1.0],[0],['0']], 'b': [[1.0],[0],['0']]})
+	m_s6 = MDP_state({'a': [[1.0],[4],['4']], 'b': [[1.0],[4],['4']]})
+	return MDP([m_s0,m_s1,m_s2,m_s3,m_s4,m_s5,m_s6],0,"MDP1_fullyobservable")
+
+def modelMDP2():
+	# x*y grid
+	# starting point : (x_init,y_init)
+	# p : prob of deplacement failure
+	x,y = 4,3
+	x_init,y_init = 0,0
+	p = 0.2
+
+	target_obs = []
+	for i in range(y):
+		target_obs.append([])	
+		if i == 0:
+			preobs = 't'
+		elif i == y-1:
+			preobs = 'b'
+		else:
+			preobs = ''
+		
+		for j in range(x):
+			if j == 0:
+				target_obs[-1].append(preobs+'l')
+			elif j == x-1:
+				target_obs[-1].append(preobs+'r')
+			elif preobs == '':
+				target_obs[-1].append('n')
+			else:
+				target_obs[-1].append(preobs)
+
+
+	states = []
+	pos = 0
+	for i in range(y):
+		for j in range(x):
+			dic = {}
+			if i == 0:
+				dic['t'] = [ [1.0], [pos], [target_obs[i][j]] ]
+			else:
+				dic['t'] = [ [1.0-p,p], [pos - x, pos], [target_obs[i-1][j],target_obs[i][j]] ]
+
+			if i == y-1:
+				dic['b'] = [ [1.0],[pos], [target_obs[i][j]] ]
+			else:
+				dic['b'] = [ [1.0-p,p],[pos + x, pos], [target_obs[i+1][j],target_obs[i][j]] ]
+
+			if j == 0:
+				dic['l'] = [ [1.0],[pos], [target_obs[i][j]] ]
+			else:
+				dic['l'] = [ [1.0-p,p],[pos - 1, pos], [target_obs[i][j-1],target_obs[i][j]] ]
+
+			if j == x-1:
+				dic['r'] = [ [1.0],[pos], [target_obs[i][j]] ]
+			else:
+				dic['r'] = [ [1.0-p,p],[pos + 1, pos], [target_obs[i][j+1],target_obs[i][j]] ]
+
+			states.append(MDP_state(dic))
+			pos += 1
+
+	return MDP(states, y_init*x+x_init,"MDP2")
+
+def modelMDP3():
+	m_s0 = MDP_state({'a': [[1.0],[0],['0']], 'b': [[1.0],[1],['1']]})
+	m_s1 = MDP_state({'a': [[0.8,0.2],[0,1],['0','1']], 'b': [[0.9,0.1],[1,0],['1','0']]})
+	return MDP([m_s0,m_s1],0,"MDP3")
+
+def modelMDP4():
+	m_s0 = MDP_state({'a': [[0.25,0.75],[2,1],['C','B']], 'b': [[1.0],[0],['A']]})
+	m_s1 = MDP_state({'b': [[1.0],[0],['B']]})
+	m_s2 = MDP_state({'a': [[1.0],[2],['C']]})
+	return MDP([m_s0,m_s1,m_s2],0,"MDP4")
+
+def modelMDP5():
+	m_s0 = MDP_state({'a': [[1.0],[1],['A']], 'b': [[1.0],[2],['A']]})
+	m_s1 = MDP_state({'a': [[0.8,0.2],[0,3],['A','C']], 'b': [[1.0],[2],['A']]})
+	m_s2 = MDP_state({'a': [[0.5,0.5],[0,1],['A','B']], 'b': [[1.0],[3],['C']]})
+	m_s3 = MDP_state({'a': [[1.0],[3],['A']], 'b': [[1.0],[0],['A']]})
+	return MDP([m_s0,m_s1,m_s2,m_s3],0,"MDP5")
+
+def modelMDP6():
+	m_s0 = MDP_state({'a': [[1.0],[1],['A']], 'b': [[1/3,2/3],[2,4],['A','A']]})
+	m_s1 = MDP_state({'a': [[0.5,1/9,7/18],[1,3,2],['A','A','A']]})
+	m_s2 = MDP_state({'a': [[1.0],[2],['A']]})
+	m_s3 = MDP_state({'a': [[1.0],[3],['A']]})
+	m_s4 = MDP_state({'a': [[0.25,0.75],[5,6],['A','A']]})
+	m_s5 = MDP_state({'a': [[1.0],[6],['A']], 'b': [[1/3,2/3],[2,7],['A','A']]})
+	m_s6 = MDP_state({'a': [[3/5,2/5],[6,5],['A','A']]})
+	m_s7 = MDP_state({'a': [[0.5,0.5],[2,3],['A','A']]})
+	return MDP([m_s0,m_s1,m_s2,m_s3,m_s4,m_s5,m_s6,m_s7],0,"MDP6")
+
+def modelMDP_smallstreet(p=0.75):
+	m_s_same = MDP_state({'m': [[p,1-p],[0,1],['M','S']], 's': [[p,1-p],[1,0],['M','S']]})
+	m_s_diff = MDP_state({'m': [[1.0],[1],['OK']],        's': [[1.0],[1],['OK']]})
+	return MDP([m_s_same,m_s_diff],0,"smallstreet")
+
+
+def modelMDP_midstreet(p=0.75):
+	m_s_rr = MDP_state({'m': [[p,1-p],[1,3],['L','R']], 's': [[p,1-p],[2,0],['L','R']]})
+	m_s_ll = MDP_state({'m': [[p,1-p],[0,2],['R','L']], 's': [[p,1-p],[3,1],['R','L']]})
+	m_s_rl = MDP_state({'m': [[1.0],[2],['OK']],        's': [[1.0],[2],['OK']]})
+	m_s_lr = MDP_state({'m': [[1.0],[3],['OK']],        's': [[1.0],[3],['OK']]})
+	return MDP([m_s_rr,m_s_ll,m_s_rl,m_s_lr],0,"midstreet")
+
+def modelMDP_bigstreet(p=0.75):
+	m_s_rr = MDP_state({'m': [[p,1-p],[1,2],['L','R']], 's': [[p,1-p],[2,0],['L','R']]})
+	m_s_ll = MDP_state({'m': [[p,1-p],[0,2],['R','L']], 's': [[p,1-p],[2,1],['R','L']]})
+	m_s_di = MDP_state({'m': [[1.0],[3],['HIT']],       's': [[1.0],[4],['OK']]})
+	m_s_de = MDP_state({'m': [[1.0],[3],['HIT']],       's': [[1.0],[3],['HIT']]})
+	m_s_vi = MDP_state({'m': [[1.0],[4],['OK']],        's': [[1.0],[4],['OK']]})
+	return MDP([m_s_rr,m_s_ll,m_s_di,m_s_de,m_s_vi],0,"bigstreet")
+
+def scheduler_uniform(actions):
+	return FiniteMemoryScheduler({0:[[1/len(actions)]*len(actions),actions]},{})
+
+def scheduler_always_same(action):
+	return FiniteMemoryScheduler({0:[[1.0],[action]]},{})
+
+# -------------------------------------
---
+++ EM_and_BW_algorithms/examples/examples_runner.py
@@ -0,0 +1,265 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from src.learning.Estimation_algorithm_MDP import *
+from src.learning.Estimation_algorithm_MCGT import *
+from src.learning.Active_Learning_MDP import Active_Learning_MDP
+from src.tools import loadSet, generateSet, saveSet, getAlphabetFromSequences, getActionsObservationsFromSequences
+from src.learning.alergia import *
+from src.learning.IOalergia import *
+from examples_models import *
+from src.models.MDP import *
+from src.models.MCGT import *
+from time import time
+
+MC = 2
+MDP = 1
+
+model = None
+alpha = None
+nb_states = None
+epsilon = None
+df = None
+lr = None
+nb_sequences = None
+nb_iteration = None
+test_set = None
+#------------------------------------------------------------------------------
+
+print("(1/17) Which kind of model do you want to learn? (1: MDP, 2: MC)")
+kind_model = int(input())
+while kind_model not in [1,2]:
+	kind_model = int(input())
+
+print("(2/17) Do you already have the training set or do you want me to generate it? (y: yes, I have the training set, n: no I don't, please generate it)")
+answer = input()
+while answer not in ['y','n']:
+	answer = input()
+
+if answer == 'y':
+	print("(3/17) Enter the location of the training set file:")
+	training_set = loadSet(input())
+	model = None
+	print("Questions 4, 5 and 6 skipped...")
+else:
+	print("(3/17) Do you want to learn a model saved in a file or to choose one from the example list? (y: yes, from a file, n: no, I would like to choose from the list)")
+	answer = input()
+	while answer not in ['y','n']:
+		answer = input()
+	if answer == 'y':
+		print("(4/17) Enter the location of the model file:\n")
+		if kind_model == MDP:
+			model = loadMDP(answer)
+		else:
+			model = loadMCGT(answer)
+	else:
+		print("(4/17) You can choose between:")
+		if kind_model == MC:
+			l = ["MC 1          (states: 5, labels: 7)",
+				 "MC 2          (states: 4, labels: 5)",
+				 "MC 3          (states: 5, labels: 5)",
+				 "MC 4          (states: 6, labels: 7)",
+				 "small game    (states: 3, labels: 6)",
+				 "REBER grammar (states: 7, labels:7)"]
+			for i in range(len(l)):
+				print(i+1," -",l[i])
+			answer = int(input())
+			while answer not in range(1,len(l)+1):
+				answer = int(input())
+			if answer == 1:
+				model = modelMCGT1()
+			elif answer == 2:
+				model = modelMCGT2()
+			elif answer == 3:
+				model = modelMCGT3()
+			elif answer == 4:
+				model = modelMCGT4()
+			elif answer == 5:
+				model = modelMCGT_game()
+			elif answer == 6:
+				model = modelMCGT_REBER()
+		else:
+			l = ["MDP 1        (states: 2, actions: 2, labels: 2)",
+				 "MDP 2        (states: 3, actions: 2, labels: 3)",
+				 "MDP 3        (states: 4, actions: 2, labels: 3)",
+				 "small street (states: 2, labels: 3)",
+				 "mid street   (states: 4, labels: 3)",
+				 "big street   (states: 5, labels: 4)"]
+			for i in range(len(l)):
+				print(i+1," -",l[i])
+			answer = int(input())
+			while answer not in range(1,len(l)+1):
+				answer = int(input())
+			if answer == 1:
+				model = modelMDP3()
+			elif answer == 2:
+				model = modelMDP4()
+			elif answer == 3:
+				model = modelMDP5()
+			elif answer == 4:
+				model = modelMDP_smallstreet()
+			elif answer == 5:
+				model = modelMDP_midstreet()
+			elif answer == 6:
+				model = modelMDP_bigstreet()
+
+
+	print("(5/17) How many sequences do you want in the training set?")
+	size_training_set = int(input())
+	print("(6/17) How many labels should contains each sequence?")
+	len_training_set = int(input())
+	print("Generating the training set...")
+	if kind_model == MDP:
+		print("(Note: since your learning an MDP I will generate the sequences using a uniform scheduler)")
+		training_set = generateSet(model,size_training_set,len_training_set,scheduler_uniform(model.actions()))
+	else:
+		training_set = generateSet(model,size_training_set,len_training_set)
+
+print("(7/17) Which learning algorithm do you want to use? ",end="")
+if kind_model == MC:
+	print("(1: MC-BW, 2: Alergia)")
+	answer = int(input())
+	while answer not in range(1,3):
+		answer = int(input())
+	algorithm = ['MC-BW','Alergia'][answer-1]
+
+else:
+	print("(1: Active MDP-BW, 2: Passive MDP-BW, 3: IOAlergia)")
+	answer = int(input())
+	while answer not in range(1,4):
+		answer = int(input())
+	algorithm = ['Active MDP-BW','Passive MDP-BW','IOAlergia'][answer-1]
+
+
+if algorithm in ["IOAlergia","Alergia"]:
+	print("(8/17) Enter the value for the alpha parameter (between 0 and 1):")
+	alpha = float(input())
+	print("Questions 9, 10, 11, 12 and 13 skipped...")
+else:
+	print("(8/17) Enter the number of states in the output model:")
+	nb_states = int(input())	
+	print("(9/17) Enter the value for the epsilon parameter (default is 0.01):")
+	limit = float(input())
+	if algorithm == "Active MDP-BW":
+		print("(10/17) Enter the value for the discount factor (default 0.9):")
+		df = float(input())
+		print("(11/17) Enter the value for the learning rate ('dynamic', 0, or a number between 0 and 1):")
+		answer = input()
+		if answer == '0':
+			lr = 0
+		elif answer == 'dynamic':
+			lr = 'dynamic'
+		else:
+			lr = float(answer)
+		print("(12/17) Enter the size of the sample for each active learning iteration:")
+		nb_sequences = int(input())
+		print("(13/17) Enter the number of active learning iterations:")
+		nb_iteration = int(input())
+	else:
+		print("Questions 10, 11, 12 and 13 skipped...")
+
+print("(14/17) Do you have a test set? (y: yes, g: no but I want you to generate one, n: no and I don't want to have one)")
+answer = input()
+while answer not in ['y','g','n']:
+	answer = input()
+if answer == 'y':
+	print("(15/17) Enter the location of your test set file:")
+	test_set = loadSet(input())
+	print("Question 16 skipped...")
+elif answer == 'g' and model == None:
+	print("I cannot generate a test set since I don't have the original model...")
+elif answer == 'g' and model != None:
+	print("(15/17) Enter the size of the test set:")
+	size_test_set = int(input())
+	print("(16/17) How many labels should contains each sequence?")
+	len_test_set = int(input())
+	print("Generating the test set...")
+	if kind_model == MDP:
+		print("(Note: since your learning an MDP I will generate the sequences using a uniform scheduler)")
+		test_set = generateSet(model,size_test_set,len_test_set,scheduler_uniform(model.actions()))
+	else:
+		test_set = generateSet(model,size_test_set,len_test_set)
+else:
+	print("Questions 15 and 16 skipped...")
+
+print("(17/17) Finally enter the output folder (it should be already created):")
+output_folder = input()
+
+
+def run_experiment(training_set,output_folder,kind_model,algorithm,test_set=None,model=None,alpha=None,nb_states=None,epsilon=None,df=None,lr=None,nb_sequences=None,nb_iteration=None):
+	saveSet(training_set,output_folder+"/training_set.txt")
+	if test_set != None:
+		saveSet(test_set,output_folder+"/test_set.txt")
+	if model != None:
+		model.save(output_folder+'/model_to_learn.txt')
+
+	f = open(output_folder+"/parameters.txt",'w')
+	f.write("Model to learn: ")
+	if model != None:
+		f.write(model.name+'\n')
+	else:
+		f.write("from training_set\n")
+	f.write("Training_set: "+str(sum(training_set[1]))+" sequences of "+str(len(training_set[0][0]))+" labels\n")
+	f.write("Algorithm: "+algorithm+'\n')
+	if algorithm in ["IOAlergia","Alergia"]:
+		f.write("alpha: "+str(alpha)+'\n')
+	else:
+		f.write("epsilon: "+str(epsilon)+'\n')
+		if algorithm == "Active MDP-BW":
+			f.write("discount factor: "+str(df)+'\n')
+			f.write("learning rate: "+str(lr)+'\n')
+			f.write("active learning sample size: "+str(nb_sequences)+'\n')
+			f.write("active learning iterations: "+str(nb_iteration)+'\n')
+
+	if model != None:
+		observations = model.observations()
+		if kind_model == MDP:
+			actions = model.actions()
+	else:
+		if kind_model == MDP:
+			actions, observations = getActionsObservationsFromSequences(training_set)
+		else:
+			observations = getAlphabetFromSequences(training_set)
+
+	start_time = time()
+
+	if algorithm == "Alergia":
+		algo = Alergia()
+		output_model = algo.learn(training_set,alpha,observations)
+		output_model.save(output_folder+'/output_model.txt')
+
+	elif algorithm =="IOAlergia":
+		algo = IOAlergia()
+		output_model = algo.learn(training_set,alpha,actions,observations)
+		output_model.save(output_folder+'/output_model.txt')
+
+	elif algorithm == "MC-BW":
+		initial_model = modelMCGT_random(nb_states,observations)
+		initial_model.save(output_folder+'/initial_model.txt')
+		algo = Estimation_algorithm_MCGT(initial_model,observations)
+		output_model = algo.learn(training_set,output_folder+'/output_model.txt',limit)
+
+	else:
+		initial_model = modelMDP_random(nb_states,observations,actions)
+		initial_model.save(output_folder+'/initial_model.txt')
+
+		if algorithm == "Passive MDP-BW":
+			algo = Estimation_algorithm_MDP(initial_model,observations,actions)
+			output_model = algo.learn(training_set,output_folder+'/output_model.txt',limit)
+		else:
+			algo = Active_Learning_MDP(initial_model,observations,actions)
+			output_model = algo.learn(training_set,df,lr,nb_sequences,nb_iteration,output_folder,limit)
+
+	delta_time = time()-start_time
+	f.write("number of states in the output model: "+str(len(output_model.states))+'\n')
+
+	f.write("Learning duration: "+str(delta_time)+'\n')
+	
+	f.write("Loglikelihood on the training set: "+str(output_model.logLikelihood(training_set))+'\n')
+	if test_set != None:
+		f.write("Loglikelihood on the test set: "+str(output_model.logLikelihood(test_set))+'\n')
+	f.close()
+
+run_experiment(training_set,output_folder,kind_model,algorithm,test_set,model,alpha,nb_states,epsilon,df,lr,nb_sequences,nb_iteration)
