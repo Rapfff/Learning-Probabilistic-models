e43776adca051afda06bc13200fc0c398218d0c6
Arnþór Logi Arnarson <arnthorla@gmail.com>
2021-10-03 15:58:51 +0000

    Some work on Experiment class, and subclass. TODO: Next: Finish logging functionality.

--- arnthorla_experiments/Experiment.py
+++ arnthorla_experiments/Experiment.py
@@ -1,4 +1,4 @@
-import os, sys, copy, math
+import os, sys, copy, math, inspect
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -12,112 +12,98 @@ from statistics import fmean, stdev
 
 class Experiment_MCGT:
     def __init__( self, num_states, num_tests, model_generators, learn_algo ):
+        self.MIN_COLUMN_WIDTH = 20                          # Minimum width of result columns
+        self.RESULTS_FOLDER_SUFFIX = "_results"             # Results folder name ends with suffix
         self.base_alphabet = "abcdefghijklmnopqrstuvwxyz"   # Base alphabet, will be sliced to needs.  
         self.num_states = num_states                        # List of integers 
         self.num_tests = num_tests                          # How many repeats of equivalent test (for statistics)
-        self.model_generators = model_generators            # List of model generator functionsj
+        self.model_generators = model_generators            # List of model generator functions
         self.learn_algo = learn_algo                        # Learning algorithim, ie Estimation Algorithm
+        self.res_headers = []                               # To have it defined in parent class, will hold header of results
+        self.res = []                                       # To have it defined in parent calss, will hold results in child
+        self.results_folder = self._fullResultsFolderName() # Full folder name of results folder
+
+    def _getDimensions(self, li, dimensions=None ):
+        if dimensions is None:
+            dimensions = []
+        if type(li) != list or not li:
+            return dimensions
+        else:
+            dimensions.append(len(li))
+            li = li[0]
+            return self._getDimensions( li, dimensions )
+
+    def _logList(self, li, dimensions, logFunc ):
+        if len(dimensions) < 1:
+            return None
+        if len(dimensions) == 1:
+            logFunc( li )
+        else:
+            length = dimensions.pop(0)
+            for i in range(length):
+                self._logList( li[i], copy.copy(dimensions), logFunc )
+
+    def _fullResultsFolderName(self):
+        current_stack_index = len(inspect.stack())-1    # index of current running script (not just this file)
+        filename = inspect.stack()[current_stack_index].filename    # full filename (with path)
+        filename = os.path.splitext( filename )[0]                  # remove file extension
+        filename = filename + self.RESULTS_FOLDER_SUFFIX            # add "_results" suffix
+        return filename
+
+    def _createResultsFolderIfNotExists(self):
+        if not os.path.isdir( self.results_folder ):
+            os.mkdir( self.results_folder )
+
+    def _logResults(self):
+        column_width = max(self.MIN_COLUMN_WIDTH, len(max(self.res_headers,key=len)))        
+        dimemsions = self._getDimensions( self.res  )
+
+        # stdout
+        def f( li, w=column_width ):
+            length = len(li)
+            for i in range(length):
+                print( f'|{li[i]:<{w}}', end='' )
+            print( '|' )
+        f( self.res_headers )
+        self._logList( self.res, dimensions, f )
+
+        # ensure that result folder exists
+        self._createResultsFolderIfNotExists()
+
+        #TODO: Next: Writing to files
+
+
 
 class Ex_Generator_Model_Trained_On_Own_Output( Experiment_MCGT ):
-    def __init__( self, num_states, num_tests, num_sequences ):
-        Experiment_MCGT.__init__( self, num_states, num_tests )
-        self.num_sequences = num_sequences                  # List of integers
-        self.res = [[[None for i in range(3)] for j in range(self.num_tests)] for k in range(len(self.num_states))]
+    def __init__( self, num_states, num_tests, model_generators, learn_algo, num_sequences ):
+        Experiment_MCGT.__init__( self, num_states, num_tests, model_generators, learn_algo )
+        self.num_sequences = num_sequences                      # List of integers
+        self.res_headers = ["#States", "#Sequences", "#Tests" ]
+        self.res = ([[[[None for i in range(3)] 
+            for j in range(self.num_tests)] 
+            for k in range(len(self.num_states))] 
+            for m in range(len(num_states))])
+
 
     def run(self):
-        for i in range( len(self.num_states) ): # States
+        for i in range( len(self.num_states) ): # Number of States
             alphabet = self.base_alphabet[:self.num_states[i]]  # slice alphabet, length equal to number of states
             len_sequence = int( math.sqrt(self.num_states[i]) ) * self.num_states[i] # formula suggested by Raphaël 
-            #TODO loop for num_sequences training sets
-            for j in range( self.num_tests ): # Tests
-                print( "[", i+1, "/",len(self.num_states),"][",j+1,"/",num_tests, "]")
-                self.res[i][j][0] = self.num_states[i]
-                original_model = modelMCGT_equiprobable( self.num_states[i], alphabet )
-                #TODO change num_sequences ... is now a list of ints
-                training_set = generateSet( original_model, num_sequences, len_per_training_sequence ) 
-                self.res[i][j][1] = original_model.logLikelihood( training_set )
-                training_model = copy.deepcopy( original_model )                    
-                algo = self.learn_algo( training_model, training_model.observations() )
-                training_model = algo.learn( training_set )
-                self.res[i][j][2] = training_model.logLikelihood( training_set )
+            for j in range( len(self.num_sequences) ): # Number of Sequnces 
+                for k in range( self.num_tests ): # Tests with same parameters
+                    print( "[",i+1,"/",len(self.num_states),"][",j+1,"/",len(self.num_sequences),"][",k+1,"/",self.num_tests,"]")
+                    self.res[i][j][k][0] = self.num_states[i]
+                    self.res[i][j][k][1] = self.num_sequences[j]
+                    original_model = self.model_generators[0]( self.num_states[i], alphabet )
+                    training_model = copy.deepcopy( original_model )                    
+                    training_set = generateSet( original_model, self.num_sequences[j], len_sequence )
+                    algo = self.learn_algo( training_model, training_model.observations() )
+                    training_model = algo.learn( training_set )
+                    self.res[i][j][k][2] = training_model.logLikelihood( training_set ) - original_model.logLikelihood( training_set )
+        super()._logResults()
+
+if __name__ == "__main__":
+    exp1 = Ex_Generator_Model_Trained_On_Own_Output([2],2,[modelMCGT_random],Estimation_algorithm_MCGT,[2])
+    exp1.run()
 
 
-#base_alphabet = "abcdefghijklmnopqrstuvwxyz" # Base alphabet, will be sliced to needs.
-#
-## Get base filename and construct filename to log results
-#base_filename = os.path.splitext( os.path.basename( __file__ ) )[0]
-#base_logfile_name = base_filename + '_results'
-#
-#
-## Definitions
-#size_alphabet = 5
-#num_training_sequences = 10
-#len_per_training_sequence = 5
-#num_states_to_test = [3]
-#num_models_per_experiment = 3 
-#
-## Experiement
-#alphabet = base_alphabet[:size_alphabet]
-#
-#res = [[[None for i in range(3)] for j in range(num_models_per_experiment)] for k in range(len(num_states_to_test))]
-#
-#for i in range( len(num_states_to_test) ):
-#    for j in range( num_models_per_experiment ):
-#        print( "[", i+1, "/",len(num_states_to_test),"][",j+1,"/",num_models_per_experiment, "]")
-#        res[i][j][0] = num_states_to_test[i]
-#        #original_model = modelMCGT_random( num_states_to_test[i], alphabet )
-#        original_model = modelMCGT_equiprobable( num_states_to_test[i], alphabet )
-#        training_set = generateSet( original_model, num_training_sequences, len_per_training_sequence ) 
-#        res[i][j][1] = original_model.logLikelihood( training_set )
-#        training_model = copy.deepcopy( original_model )                    
-#        algo = Estimation_algorithm_MCGT( training_model, training_model.observations() )
-#        training_model = algo.learn( training_set )
-#        res[i][j][2] = training_model.logLikelihood( training_set )
-#        #res[i][j][3] = res[i][j][2] - res[i][j][1]
-#
-#model_name = original_model.__str__()
-#model_name = model_name.split("_")
-#print("original_model: ", "_".join(model_name[:-1]) )
-#
-## Summary statistics
-#summary = [[None for i in range(2)] for j in range(len(num_states_to_test))]
-#for i in range( len(num_states_to_test) ):
-#    results = []
-#    for j in range( num_models_per_experiment ):
-#       results.append( res[i][j][2]-res[i][j][1] )
-#    summary[i][0] = fmean( results )
-#    summary[i][1] = stdev( results )
-#
-## Print Results
-#print()
-#for model in res:
-#    for test in model: 
-#        print( "#states: ", test[0], "\toriginal: ", test[1], "\ttrained: ", test[2], "\timprovement: ", test[2]-test[1] )
-#        #print(  test[0], "\t", test[1], "\t ", test[2], "\t",  test[3] )
-#
-## Print Summary statistics
-#print()
-#for i in range( len( num_states_to_test ) ):
-#    print( "#states: ", num_states_to_test[i], "\tMean impovement: ", summary[i][0], "\tStdev of improvement: ", summary[i][1] )
-#
-## Log results to file
-#with open( base_logfile_name + ".txt", "a" ) as f:
-#    print( "Size of observation alphabet: ", size_alphabet, file=f )
-#    print( "Number of training sequences: ", num_training_sequences, file=f )
-#    print( "Length per training sequence: ", len_per_training_sequence, file=f )
-#    print( "Number of states to be tested: ", num_states_to_test, file=f )
-#    print( "Number of models that are tested in each experiment: ", num_models_per_experiment, file=f )
-#    print( "Observation alphabet: ", alphabet, file=f )
-#    print(file=f)
-#    for model in res:
-#        for test in model: 
-#            print( "#states: ", test[0], "\toriginal: ", test[1], "\ttrained: ", test[2], "\timprovement: ", test[2]-test[1], file=f )
-#    # Print Summary statistics
-#    print(file=f)
-#    for i in range( len( num_states_to_test ) ):
-#        print( "#states: ", num_states_to_test[i], "\tMean impovement: ", summary[i][0], "\tStdev of improvement: ", summary[i][1], file=f )
-#
-#
-#
-#
-#
--- arnthorla_experiments/test.py
+++ arnthorla_experiments/test.py
@@ -1,10 +1,11 @@
-import os, sys
+import os, sys, copy
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from examples.examples_models import *
-from src.learning.Estimation_algorithm_HMM import *
+from EM_and_BW_algorithms.examples.examples_models import *
+from EM_and_BW_algorithms.src.learning.Estimation_algorithm_HMM import *
+from Experiment import *
 #from ..src.models.HMM import *
 
 #model = modelHMM1()
@@ -14,27 +15,83 @@ from src.learning.Estimation_algorithm_HMM import *
 #model = modelHMM2_equiprobable()
 #model = modelHMM3()
 #model = modelHMM4()
-model = modelHMM_random(10, "$abcdxy")
-model.pprint()
-observations = model.observations()
-algo = Estimation_algorithms_HMM(model,observations)
+#model = modelHMM_random(10, "$abcdxy")
+#model.pprint()
+#observations = model.observations()
+#algo = Estimation_algorithms_HMM(model,observations)
+#
+#seq = "$bd"
+#p1 = algo.problem1(seq)
+#print( p1 )
+#p2 = algo.problem2(seq)
+#print(p2)
+#
+#print()
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
+#print( model.run(10) )
 
-seq = "$bd"
-p1 = algo.problem1(seq)
-print( p1 )
-p2 = algo.problem2(seq)
-print(p2)
 
-print()
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
-print( model.run(10) )
+#def getDimensions( li, dimensions=None ):
+#    if dimensions is None:
+#        dimensions = []
+#    if type(li) != list:
+#        return dimensions
+#    else:
+#        dimensions.append(len(li))
+#        li = li[0]
+#        return getDimensions( li, dimensions )
+#
+#def logList( li, dimensions, logFunc ):
+#    if len(dimensions) == 1:
+#        logFunc( li )
+#    else:
+#        length = dimensions.pop(0)
+#        for i in range(length):
+#            logList( li[i], copy.copy(dimensions), logFunc )
+#
+#
+#
+#res = ([[[[None for i in range(4)] 
+#            for j in range(3)] 
+#            for k in range(10)]
+#            for m in range(5)])
+##print()
+##print( getDimensions( res ) )    
+#
+#for i in range(5):
+#    for j in range(10):
+#        print('[')
+#        for k in range(3):
+#                res[i][j][k][0] = i
+#                res[i][j][k][1] = j
+#                res[i][j][k][2] = k
+#                print("[",i,",",j,",",k,"]","t[",k,"]: ", res[i][j][k])
+#        print(']')
+#
+#print()
+##print( getDimensions( res ) )
+#dim = getDimensions( res )
+#print( dim )    
+#print()
+#
+#logList( res, dim, print )
+#print()
 
+#tmp = [ "blabla", "baba", "la" ]
+#print( tmp )
+#tmp2 = map( len, tmp )
+#tmp3 = list(tmp2)
+#print( tmp3 )
+#print( max( 7,len(max( tmp, key=len ))))
+
+exp1 = Ex_Generator_Model_Trained_On_Own_Output([13],2,[modelMCGT_random],Estimation_algorithm_MCGT,[2])
+exp1.run()
