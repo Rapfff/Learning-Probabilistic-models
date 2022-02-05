3809f915ef3ebcb328d00d01479ca84ae1deffd6
Rapfff <23459708+Rapfff@users.noreply.github.com>
2022-02-05 10:01:50 +0000

    Add files via upload

--- EM_and_BW_algorithms/test.py
+++ EM_and_BW_algorithms/test.py
@@ -7,7 +7,7 @@ original = modelCOMC1()
 training_set = generateSet(original,1,4)
 
 
-m = modelCOMC_random(2)
+m = modelCOMC_random(2,min_mu=0.0)
 algo = Estimation_algorithm_coMC(m)
 
 mout = algo.learn(training_set)
