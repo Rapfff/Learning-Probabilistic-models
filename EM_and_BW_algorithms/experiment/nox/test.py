b4c9ad2789ef2ef14694fdbd3c430fe45f211bca
kraken_laptop <raphal20@ru.is>
2022-03-04 13:14:10 +0000

    Working with SFA (2)

--- EM_and_BW_algorithms/experiment/nox/test.py
+++ EM_and_BW_algorithms/experiment/nox/test.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statistics import mean, stdev
 from scipy.signal import hilbert
-from math import exp
+from math import exp, sqrt
 from experiment.nox.edfreader import EDFreader
 from src.tools import saveSet, loadSet, setFromList, randomProbabilities
 from src.learning.BW_HMM import BW_HMM
@@ -27,63 +27,15 @@ SIZE_ALPHABET = 10
 SIGNAL_ID = 44
 SIGNAL_NAME = "F3_M2"
 
-WINDOW_SIZE_SEC = 1
-EVALUATING_WINDOW_SIZE_SEC = 15
+WINDOW_SIZE_SEC_MAX = 10
 
 NB_STATES = 5
-
-def clean(s):
-    i = 1
-    while i < len(s):
-        if s[i] != s[0]:
-            break
-        i+=1
-
-    if i/len(s) > 0.02:
-        s = s[i:]
-
-    if len(s) > 2:
-        i = len(s)-2
-        while i >= 0:
-            if s[i] != s[-1]:
-                break
-            i-=1
-        if 1.0-i/len(s) > 0.02:
-            s = s[:i+1]
-
-    return s
 
 def normalize(seq):
 	mu  = mean(seq)
 	std = stdev(seq)
 	return [(i-mu)/std for i in seq]
 
-def peak_to_peak_amplitude_and_freq(s):
-	m = sum(s)/len(s)
-	s = [i-m for i in s if i-m != 0]
-	peaks_size = []
-	up   = []
-	down = []
-	c = 1
-	if s[0] > 0:
-		up.append(s[0])
-	else:
-		down.append(s[0])
-	i = 1
-	while i < len(s):
-		if s[i-1]*s[i] < 0 and len(up)>0 and len(down)>0:
-			c +=1
-			peaks_size.append(mean(up)-mean(down))
-			up = []
-			down = []
-		if s[i] > 0:
-			up.append(s[i])		
-		if s[i] < 0:
-			down.append(s[i])
-		i += 1
-	if len(up)>0 and len(down)>0:
-		peaks_size.append(mean(up)-mean(down))
-	return (mean(peaks_size),c,m)
 
 def read_files():
 	events = open(EVENT_FILE,'r')
@@ -94,93 +46,45 @@ def read_files():
 	r = EDFreader(EDF_FILE)
 	length = r.getTotalSamples(SIGNAL_ID)
 	frequency = r.getSampleFrequency(SIGNAL_ID)
-	exp_duration = length/r.getSampleFrequency(SIGNAL_ID) #in seconds
+	exp_duration = length/frequency #in seconds
 
 	start_time_event = [int(i[:2]) for i in l.split(';')[2].split(':')]
 	start_time_edf   = [r.getStartTimeHour(),r.getStartTimeMinute(),r.getStartTimeSecond()]
 	diff = (start_time_event[0] - start_time_edf[0])*60*60 + (start_time_event[1] - start_time_edf[1])*60 + (start_time_event[2] - start_time_edf[2])
 	begining = int(diff*frequency)
-	window_size = int(WINDOW_SIZE_SEC*frequency)
 	#begining = 4134971
-	window = np.arange(window_size,dtype=np.float_)
 	r.fseek(SIGNAL_ID,begining,EDFreader.EDFSEEK_SET)
-
-	#amp  = []
-	#freq = []
-	#means= []
+	
 	data = []
-
-	while r.ftell(SIGNAL_ID)+window_size < length:
-		window = np.arange(window_size,dtype=np.float_)
-		r.readSamples(SIGNAL_ID,window,window_size)
-		#window = clean(window)
-		#if len(window)>0:
-		#ta,tf,tm = peak_to_peak_amplitude_and_freq(window)
-		#amp.append(ta)
-		#freq.append(tf)
-		#means.append(tm)
-		data.append([i for i in window])
-
 	stages = []
-	x_stages = {}
-	l = events.readline()
-	c = 1
+	x_stages = []
 	while l:
 		l = l.split(';')
+		l[1] = int(l[1])
 		s = l[0]
-		d = int(l[1])//WINDOW_SIZE_SEC
 		if not s in stages:
 			stages.append(s)
-			x_stages[s] = []
-		#x_stages[s].append([c+i for i in range(-1,d,1)])
-		for i in range(-1,d,1):
-			x_stages[s].append(c+i)
+
+		nb_windows = max(l[1]//WINDOW_SIZE_SEC_MAX,1)
+		windows_sizes = [ l[1]//nb_windows for i in range(nb_windows) ]
+		diff = l[1] - sum(windows_sizes)
+		for i in range(diff):
+			windows_sizes[i] += 1
+
+		for size in windows_sizes:		
+			window = np.arange(int(size*frequency),dtype=np.float_)
+			r.readSamples(SIGNAL_ID,window,size)
+			data.append([i for i in window])
+			x_stages.append(s)
+
 		l = events.readline()
-		c += d
+
+
+	#while r.ftell(SIGNAL_ID)+window_size < length:
+	
 	events.close()
 	return [data,stages,x_stages]
 
-def naive_analysis():
-	hil, stages, x_stages = read_files()
-	hil = normalize(hil)
-
-	hil = [(i-mu)/std for i in hil] #normalizing
-	mu   = {}
-	var  = {}
-	trans= {}
-	for s in stages:
-		mu[s]    = 0.0
-		var[s]   = 0.0
-		trans[s] = [0.0 for i in range(len(stages))]
-
-	for s in stages:
-		if 0 in x_stages[s]:
-			break
-
-	for step in range(len(hil)-1):
-		mu[s] += hil[step]
-		for ss in stages:
-			if step+1 in x_stages[ss]:
-				break
-		trans[s][stages.index(ss)] += 1
-		s = ss
-	mu[s] += hil[-1]
-	for s in stages:
-		mu[s] /= len(x_stages[s])
-		trans[s] = [j/len(x_stages[s]) for j in trans[s]]
-
-	for step in range(len(hil)-1):
-		for s in stages:
-			if step+1 in x_stages[s]:
-				break
-		var[s] += (hil[step]-mu[s])**2
-	for s in stages:
-		var[s] /= len(x_stages[s])
-		print("\nSTAGE",s,"*****************")
-		print("Mean    :",round(mu[s],5))
-		print("Variance:",round(var[s],5))
-		for ss in stages:
-			print(s,"=>",ss,":",round(100*trans[s][stages.index(ss)],5),"%")
 	
 def write_training_test_set(fraction_test,name='',n_coefs=6,n_bins=6):
 	"""name is the name of the output files,
@@ -189,11 +93,35 @@ def write_training_test_set(fraction_test,name='',n_coefs=6,n_bins=6):
 		name = '_'+str(name)
 	data, stages, x_stages = read_files()
 	transformer = SymbolicFourierApproximation(n_coefs=n_coefs,n_bins=n_bins)
-	seqs = transformer.fit_transform(data)
-	input(seqs)
-	shuffle(seqs)
-	test_seqs = seqs[:int(fraction_test*len(seqs))]
-	training_seqs = seqs[int(fraction_test*len(seqs)):]
+	data = transformer.fit_transform(data)
+
+
+	"""
+	coords = [[] for i in stages]
+	set_seq = []
+	for i in range(len(data)):
+		seq = list(data[i])
+		if not seq in set_seq:
+			set_seq.append(seq)
+			for s in range(len(stages)):
+				coords[s].append(0)
+		j = set_seq.index(seq)
+		coords[stages.index(x_stages[i])][j] += 1
+	dist = []
+	for s in range(len(stages)-1):
+		#dist.append([])
+		for ss in range(s+1,len(stages)):
+			print(stages[s], " - ",stages[ss], " ",sqrt(sum([(coords[s][i]-coords[ss][i])**2 for i in range(len(set_seq))])))
+			#dist[-1].append(sqrt(sum([(coords[s][i]-coords[ss][i])**2 for i in range(len(set_seq))])))
+	"""
+
+	l = [i for i in range(len(data))]
+	data_shuffled = [ data[i] for i in l]
+	x_stages_shuffled = [ x_stages[i] for i in l]
+
+
+	test_seqs = data_shuffled[:int(fraction_test*len(data_shuffled))]
+	training_seqs = data_shuffled[int(fraction_test*len(data_shuffled)):]
 
 	training_set = setFromList(training_seqs)
 	test_set = setFromList(test_seqs)
@@ -201,17 +129,16 @@ def write_training_test_set(fraction_test,name='',n_coefs=6,n_bins=6):
 	saveSet(training_set,"training_set"+name+".txt")
 	saveSet(test_set,"test_set"+name+".txt")
 
-#IDEE:
 
 n_bins = 6
-write_training_test_set(0.1,n_bins=n_bins)
+#write_training_test_set(0.1,n_bins=n_bins)
 
-tr = loadSet("training_set.txt",True)
-ts = loadSet("test_set.txt",True)
+tr = loadSet("training_set.txt")
+ts = loadSet("test_set.txt")
 
-rm = modelHMM_random(5,[chr(i) for i in range(97,97+n_bins)])
-rm.save("init_model.txt")
-rm = loadcoHMM("init_model.txt")
+#rm = modelHMM_random(5,[chr(i) for i in range(97,97+n_bins)])
+#rm.save("init_model.txt")
+rm = loadHMM("init_model.txt")
 
 algo = BW_HMM(rm)
 out = algo.learn(tr,verbose=True)
@@ -219,75 +146,3 @@ out.pprint()
 
 print("Loglikelihood on test_set for initial model ",rm.logLikelihood(ts))
 print("Loglikelihood on test_set for output  model ",out.logLikelihood(ts))
-
-def write_training_sets_each_stage():
-	hil, stages, x_stages = read_files()
-	hil = normalize(hil)
-	for s in stages:
-		for j in x_stages[s]:
-			seq = []
-			t   = []
-			for i in range(0,len(j),EVALUATING_WINDOW_SIZE_SEC):
-				ss = j[i:min(len(j),i+EVALUATING_WINDOW_SIZE_SEC)]
-				ss = [hil[k] for k in ss]
-				if ss in seq:
-					t[seq.index(ss)] += 1
-				else:
-					seq.append(ss)
-					t.append(1)
-		saveSet([seq,t],str(s)+"_training.txt")
-
-	seqs = []
-	for i in range(len(hil)//EVALUATING_WINDOW_SIZE_SEC):
-		seqs.append(hil[i*EVALUATING_WINDOW_SIZE_SEC:(i+1)*EVALUATING_WINDOW_SIZE_SEC])
-	shuffle(seqs)
-	test_seqs = seqs[:int(fraction_test*len(seqs))]
-	test_set = setFromList(test_seqs)
-	saveSet(test_set,"test_set"+name+".txt")
-
-
-
-def printing_stuffs(stages,x_stages,amp,freq,means):
-	fig, ax = plt.subplots()
-	for s in stages:
-		first = True
-		for x in x_stages[s]:
-			if first:
-				first = False
-				plt.plot(x,[amp[i] for i in x],color=colors[stages.index(s)],label=s)
-			else:
-				plt.plot(x,[amp[i] for i in x],color=colors[stages.index(s)])
-	ax.legend()
-	ax.set_title("Amplitude "+SIGNAL_NAME)
-	plt.show()
-
-	fig, ax = plt.subplots()
-	for s in stages:
-		first = True
-		for x in x_stages[s]:
-			if first:
-				first = False
-				plt.plot(x,[freq[i] for i in x],color=colors[stages.index(s)],label=s)
-			else:
-				plt.plot(x,[freq[i] for i in x],color=colors[stages.index(s)])
-	ax.legend()
-	ax.set_title("Frequency "+SIGNAL_NAME)
-	plt.show()
-
-
-	fig, ax = plt.subplots()
-	for s in stages:
-		first = True
-		for x in x_stages[s]:
-			if first:
-				first = False
-				plt.plot(x,[means[i] for i in x],color=colors[stages.index(s)],label=s)
-			else:
-				plt.plot(x,[means[i] for i in x],color=colors[stages.index(s)])
-	ax.legend()
-	ax.set_title("Mean "+SIGNAL_NAME)
-	plt.show()
-	#plt.plot(range(window_size),window)
-	#plt.plot(range(window_size),[sum(window)/window_size for i in range(window_size)])
-	#plt.show()
-	#print(peak_to_peak_amplitude_and_freq(window))
