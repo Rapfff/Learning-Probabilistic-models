0627de97a5dc61973b48fc2cb5c823e9f5c76de6
kraken_laptop <raphal20@ru.is>
2022-03-25 15:59:07 +0100

    corrections addStates

--- EM_and_BW_algorithms/test_LTL.py
+++ EM_and_BW_algorithms/test_LTL.py
@@ -1,7 +1,7 @@
 from examples.examples_models import modelMCGT_REBER, modelMCGT_random
 from src.learning.BW_LTL import BW_LTL
 from src.tools import generateSet
-
+from src.models.MCGT import loadMCGT
 m = modelMCGT_REBER()
 
 training_set = generateSet(m,1000,7)
