9b76410138d35b8bf980b36a8e63f54a4e18d610
kraken_laptop <raphal20@ru.is>
2022-03-29 17:27:42 +0200

    correction 5

--- EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
+++ EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
@@ -114,7 +114,7 @@ n_coefs= 4
 training_psgs = list(range(1,41))
 training_psgs.remove(21)
 
-tr = write_training_test_set(list(range(1,41)),0.0,n_bins=n_bins,n_coefs=n_coefs)
+tr = write_training_test_set(training_psgs,0.0,n_bins=n_bins,n_coefs=n_coefs)
 print("number of traces:",sum(tr[1]))
 #tr = loadSet("training_set.txt")
 
