0977ac16a7354727b015a27c7885e9bbacbf4725
kraken_laptop <raphal20@ru.is>
2022-03-30 11:05:00 +0200

    correction 9

--- EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
+++ EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
@@ -118,12 +118,11 @@ def evaluation(m: HMM, psg_numbers: list):
 					summ = 0.0
 					for ss in range(len(m.states)):
 						p = m.tau(ss,s,h[t])
-						summ += alpha_matrix[ss][t]*p
+						summ += alpha_matrix[ss][-1]*p
 					alpha_matrix[s].append(summ)
 				alphas = [ alpha_matrix[s][-1] for s in range(len(m.states)) ]
 				chosen = alphas.index(max(alphas))
 				corr_matrix[chosen][sleep_stages.index(h[t])] += 1
-	
 	return corr_matrix
 
 
