23cf1d41d8e2fabce50e9b4387aaee7b529cbd60
kraken_laptop <raphal20@ru.is>
2022-04-08 16:54:54 +0000

    Learning for all EEG

--- EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
+++ EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
@@ -12,17 +12,13 @@ from src.models.HMM import loadHMM, HMM
 from examples.examples_models import modelHMM_random
 import numpy as np
 from pyts.approximation import SymbolicFourierApproximation
-from itertools import product
 from random import shuffle
 import pandas as pd
 
-SIGNAL_ID = 44
-SIGNAL_NAME = "F3_M2"
-
 MANUAL_SCORING_WINDOW_SEC = 30
 
-WINDOW_SIZE_SEC = 10 #nb of sec as input to DFA
-NB_WINDOWS_BY_SEQ = 6 #nb of sec by sequence = WINDOW_SIZE_SEC_MAX*NB_WINDOWS_BY_SEQ
+WINDOW_SIZE_SEC = 30 #nb of sec as input to DFA
+NB_WINDOWS_BY_SEQ = 180 #nb of sec by sequence = WINDOW_SIZE_SEC_MAX*NB_WINDOWS_BY_SEQ
 
 NB_STATES = 5
 
@@ -38,9 +34,9 @@ def file_paths_from_psg_number(nb):
 	hypno_file = "/datasets/10x50_psg/raw_event_exports/01/psg"+nb+"/xls_hypnogram.xls"
 	return [edf_file, hypno_file]
 
-def read_EDF_signal(r,size):
+def read_EDF_signal(r,size,signal_id):
 	window = np.arange(size,dtype=np.float_)
-	r.readSamples(SIGNAL_ID,window,WINDOW_SIZE_SEC)
+	r.readSamples(signal_id,window,WINDOW_SIZE_SEC)
 	return [i for i in window]
 
 def find_starting_point(r,frequency,hypno_file):
@@ -54,28 +50,28 @@ def find_starting_point(r,frequency,hypno_file):
 	begining = int(diff*frequency)
 	return begining
 
-def read_files(psg_number: int):
+def read_files(psg_number: int, signal_id: int):
 	edf_file , hypno_file = file_paths_from_psg_number(psg_number)
 	
 	r = EDFreader(edf_file)
-	length = r.getTotalSamples(SIGNAL_ID)
-	frequency = r.getSampleFrequency(SIGNAL_ID)
+	length = r.getTotalSamples(signal_id)
+	frequency = r.getSampleFrequency(signal_id)
 	exp_duration = length/frequency #in seconds
 
 	begining = find_starting_point(r,frequency,hypno_file)
 	
-	r.fseek(SIGNAL_ID,begining,EDFreader.EDFSEEK_SET)
+	r.fseek(signal_id,begining,EDFreader.EDFSEEK_SET)
 	data = []
 	c = 0
 	while (c+1)*WINDOW_SIZE_SEC < exp_duration:
-		data.append(read_EDF_signal(r,int(WINDOW_SIZE_SEC*frequency)))
+		data.append(read_EDF_signal(r,int(WINDOW_SIZE_SEC*frequency),signal_id))
 		c += 1
 	
-	data.append(read_EDF_signal(r,int((exp_duration-c*WINDOW_SIZE_SEC)*frequency)))
+	data.append(read_EDF_signal(r,int((exp_duration-c*WINDOW_SIZE_SEC)*frequency),signal_id))
 	return data
 
 
-def write_set(psg_numbers: list,name,n_coefs=4,n_bins=6,shuffling=True):
+def write_set(psg_numbers: list,signal_id,name,n_coefs=4,n_bins=6,shuffling=True):
 	"""name is the name of the output files,
 	fraction_test is a float between ]0,1[ corresponding to the fraction of sequences in the test set """
 	
@@ -83,7 +79,7 @@ def write_set(psg_numbers: list,name,n_coefs=4,n_bins=6,shuffling=True):
 	
 	for psg_number in psg_numbers:
 		print("PSG number:",psg_number)
-		data = read_files(psg_number)
+		data = read_files(psg_number,signal_id)
 		transformer = SymbolicFourierApproximation(n_coefs=n_coefs,n_bins=n_bins)
 		data = transformer.fit_transform(data)
 
@@ -102,7 +98,7 @@ def write_set(psg_numbers: list,name,n_coefs=4,n_bins=6,shuffling=True):
 		saveSet(data, name+".txt")
 	return data
 
-def evaluation(m: HMM, psg_numbers: list) -> list:
+def evaluation(m: HMM, signal_id, psg_numbers: list) -> list:
 	sleep_stages = ["Wake","N1","N2","N3","REM"]
 	bw = BW(m)
 	corr_matrix = []
@@ -112,7 +108,7 @@ def evaluation(m: HMM, psg_numbers: list) -> list:
 	for psg_number in psg_numbers:
 		h = pd.read_excel(file_paths_from_psg_number(psg_number)[1])
 		h = list(h["Event"])[1:]
-		g = write_set([psg_number],False,shuffling=False)
+		g = write_set([psg_number],signal_id,False,shuffling=False)
 
 		for seq in range(len(g[0])):
 			alphas = bw.computeAlphas(g[0][seq])
@@ -124,49 +120,53 @@ def evaluation(m: HMM, psg_numbers: list) -> list:
 				if index_h >= len(h):
 					break
 				if h[index_h] in sleep_stages:
-					# chosen = alphas_betas.index(max(alphas_betas))
-					# corr_matrix[chosen][sleep_stages.index(h[index_h])] += g[1][seq]
-					for s in range(len(m.states)):
-						corr_matrix[s][sleep_stages.index(h[index_h])] += g[1][seq]*alphas_betas[s]
+					chosen = alphas_betas.index(max(alphas_betas))
+					corr_matrix[chosen][sleep_stages.index(h[index_h])] += g[1][seq]
+					#for s in range(len(m.states)):
+					#	corr_matrix[s][sleep_stages.index(h[index_h])] += g[1][seq]*alphas_betas[s]
 	return corr_matrix
 
 
 
-n_bins = 5 #nb of letters
-n_coefs= 4
+n_bins  = 5 # nb of letters. 5 because delta, theta, alpha, beta1, beta2 activity
+alphabet = list("abcdefghijklmnopqrstuvwxyz")[:n_bins]
+n_coefs = 4 # 5 because delta, theta, alpha, beta1, beta2 activity
+
+list_signals = [     20,     24,     30,    34,      44,     48,     67,     71] 
+signals_name = ["C3-M2","C4-M1","E1-M2","E2-M1","F3-M2","F4-M1","O1-M2","O2-M1"]
+
 
 # size alphabet = n_bins**n_coefs
 # nb possible sequences = (size alphabet)**NB_WINDOWS_BY_SEQ 
+psgs = list(range(1,51))
+psgs.remove(21)
+shuffle(psgs)
+training_psgs = psgs[:44]
+test_psgs = psgs[44:]
 
-training_psgs = list(range(1,41))
-training_psgs.remove(21)
-test_psgs = list(range(41,51))
+for signal_index in range(len(list_signals)):
+	signal_id = list_signals[signal_index]
+	signal_name = signals_name[signal_index]
+	write_set(training_psgs,signal_id,"training_set",n_coefs,n_bins)
+	write_set(test_psgs,signal_id,"test_set",n_coefs,n_bins)
+	tr = loadSet("training_set.txt")
+	ts = loadSet("test_set.txt")
+	rm = modelHMM_random(NB_STATES,alphabet,random_initial_state=True)
+	algo = BW_HMM(rm)
+	out = algo.learn(tr, output_file="model_"+signal_name, verbose=True)
 
-# write_set(training_psgs,"training_set",n_coefs,n_bins)
-# write_set(test_psgs,"test_set",n_coefs,n_bins)
-
-# tr = loadSet("training_set.txt")
-# ts = loadSet("test_set.txt")
- 
-# alphabet = [''.join(j) for j in list(product(*[[chr(i) for i in range(97,97+n_bins)]]*n_coefs))]
- 
-# rm = modelHMM_random(NB_STATES,alphabet,random_initial_state=True)
-
-# algo = BW_HMM(rm)
-# out = algo.learn(tr, verbose=True)
-# out.pprint()
-# out.save("output_model.txt")
-
-out = loadHMM("output_model.txt")
-corr_matrix = evaluation(out, test_psgs)
-
-print(" "*8+'|  Wake  |   N1   |   N2   |   N3   |  REM   ',end='')
-for i in range(len(corr_matrix)):
-	row = corr_matrix[i]
-	print('\n'+'-'*53)
-	print("   s"+str(i)+"   ",end="")
-	for j in row:
-		s = str(j)
-		print('|'+" "*(8-len(s))+s,end="")
-print('\n'+'-'*53)
+	corr_matrix = evaluation(out, signal_id, test_psgs)
+	string  = signal_name+'\n'
+	string += " "*8+'|  Wake  |   N1   |   N2   |   N3   |  REM   '
+	for i in range(len(corr_matrix)):
+		row = corr_matrix[i]
+		string += '\n'+'-'*53+'\n'
+		string += "   s"+str(i)+"   "
+		for j in row:
+			s = str(j)
+			string += '|'+" "*(8-len(s))+s
+	string += '\n'+'-'*53+'\n'
+	f = open("report_"+signal_name+".txt",'w')
+	f.write(string)
+	f.close()
 
