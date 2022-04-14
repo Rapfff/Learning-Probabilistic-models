f03610dd0c6b267242d60f766cd23f8636dc9d9f
kraken_university <raphal20@ru.is>
2022-04-14 13:52:49 +0000

    results for experiment REBER

--- EM_and_BW_algorithms/experiment/CTMC_composition/main.py
+++ EM_and_BW_algorithms/experiment/CTMC_composition/main.py
@@ -9,6 +9,7 @@ from src.learning.BW_CTMC_Composition import BW_CTMC_Composition
 from src.tools import generateSet
 from statistics import mean, stdev
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 def experiment(original_model, name, nb_states_small, nb_states_big,nb_experiments):
     running_time = [[],[],[]]
@@ -16,7 +17,8 @@ def experiment(original_model, name, nb_states_small, nb_states_big,nb_experimen
 
     training_set = generateSet(original_model,1000,7)
     test_set     = generateSet(original_model,1000,7)
-    """
+    
+    f = open("points_Composition_"+name+".txt",'a')
     for c in range(nb_experiments):
         print("Composition ",c+1,"/",nb_experiments," ",name,sep="")
         s = datetime.now()
@@ -26,7 +28,11 @@ def experiment(original_model, name, nb_states_small, nb_states_big,nb_experimen
         output_model1 = output_model1.toMC(name)
         running_time[0].append((datetime.now()-s).total_seconds())
         quality[0].append(output_model1.logLikelihood(test_set))
-    """
+        f.write(str(running_time[0][-1])+'\n')
+        f.write(str(quality[0][-1])+'\n')
+    f.close()
+    
+    f = open("points_classicalbig_"+name+".txt",'a')
     for c in range(nb_experiments):
         print("Classic big",c+1,"/",nb_experiments," ",name,sep="")
         s = datetime.now()
@@ -34,7 +40,11 @@ def experiment(original_model, name, nb_states_small, nb_states_big,nb_experimen
         output_model2 = BW_MC(r3).learn(training_set)
         running_time[1].append((datetime.now()-s).total_seconds())
         quality[1].append(output_model2.logLikelihood(test_set))
-    """
+        f.write(str(running_time[1][-1])+'\n')
+        f.write(str(quality[1][-1])+'\n')
+    f.close()
+    
+    f = open("points_classicalsmall_"+name+".txt",'a') 
     for c in range(nb_experiments):
         print("Classic equiv",c+1,"/",nb_experiments," ",name,sep="")
         s = datetime.now()
@@ -42,12 +52,15 @@ def experiment(original_model, name, nb_states_small, nb_states_big,nb_experimen
         output_model2 = BW_MC(r3).learn(training_set)
         running_time[2].append((datetime.now()-s).total_seconds())
         quality[2].append(output_model2.logLikelihood(test_set))
-    """
-    #return ([[[mean(running_time[i]),stdev(running_time[i])] for i in range(3)],[[mean(quality[i]),stdev(quality[i])] for i in range(3)]])
-    return [[mean(running_time[1]),stdev(running_time[1])],[mean(quality[1]),stdev(quality[1])]]
+        f.write(str(running_time[2][-1])+'\n')
+        f.write(str(quality[2][-1])+'\n')
+    f.close()
+    
+    return (running_time,quality)
 
+nb_experiments = 0
+running_time, quality = experiment(modelMC_REBER(),"REBER",4,16,nb_experiments)
 """
-running_time, quality = experiment(modelMC_REBER(),"REBER",4,16,20)
 string1 = ""
 string1 += "Model: REBER"+'\n'
 string1 += "Average running time composition: "+str(running_time[0][0])+'\n'
@@ -68,20 +81,52 @@ string1 += '\n'
 string1 += "Average loglikelihood classical small: "+str(quality[2][0])+'\n'
 string1 += "Std loglikelihood classical small: "+str(quality[2][1])+'\n'
 string1 += '\n'
-print()
-print(string1)
-"""
 running_time, quality = experiment(modelMC_map(),"MAP",5,25,10)
-string2 = ""
-string2 += "Average running time classical big: "+str(running_time[0])+'\n'
-string2 += "Std running time classical big: "+str(running_time[1])+'\n'
-string2 += "\n"
-string2 += "Average loglikelihood classical big: "+str(quality[0])+'\n'
-string2 += "Std loglikelihood classical big: "+str(quality[1])+'\n'
-string2 += '\n'
-print()
-print(string2)
-
-f = open("report2.txt",'w')
+string1 += "Model: MAP"+'\n'
+string1 += "Average running time composition: "+str(running_time[0][0])+'\n'
+string1 += "Std running time composition: "+str(running_time[0][1])+'\n'
+string1 += "\n"
+string1 += "Average running time classical big: "+str(running_time[1][0])+'\n'
+string1 += "Std running time classical big: "+str(running_time[1][1])+'\n'
+string1 += "\n"
+string1 += "Average running time classical small: "+str(running_time[2][0])+'\n'
+string1 += "Std running time classical small: "+str(running_time[2][1])+'\n'
+string1 += "\n"
+string1 += "Average loglikelihood composition: "+str(quality[0][0])+'\n'
+string1 += "Std loglikelihood composition: "+str(quality[0][1])+'\n'
+string1 += "\n"
+string1 += "Average loglikelihood classical big: "+str(quality[1][0])+'\n'
+string1 += "Std loglikelihood classical big: "+str(quality[1][1])+'\n'
+string1 += '\n'
+string1 += "Average loglikelihood classical small: "+str(quality[2][0])+'\n'
+string1 += "Std loglikelihood classical small: "+str(quality[2][1])+'\n'
+string1 += '\n'
+f = open("report3.txt",'w')
 f.write(string2)
 f.close()
+"""
+
+methods = ["Composition","Classical big","Classical small"]
+
+running_time = [[],[],[]]
+quality = [[],[],[]]
+
+for i,n in enumerate(["Composition","classicalbig","classicalsmall"]):
+    f = open("points_"+n+"_REBER.txt")
+    l = f.readline()
+    while l != "":
+        running_time[i].append(float(l[:-1]))
+        l = f.readline()
+        quality[i].append(float(l[:-1]))
+        l = f.readline()
+    f.close()
+
+# size and color:
+colors = ['r','b','g']
+fig, ax = plt.subplots()
+
+for i in range(3):
+    ax.scatter(running_time[i], quality[i],c=colors[i],label=methods[i], alpha=0.3,s=45)
+ax.legend()
+ax.set(xlabel='running time (s)', ylabel='average loglikelighood')
+plt.show()
