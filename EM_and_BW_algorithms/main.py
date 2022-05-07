914b4f07dd7a4c4b1f3320122383a14b78df1ae1
kraken_laptop <raphal20@ru.is>
2022-05-07 16:26:05 +0200

    Moving main.py

--- EM_and_BW_algorithms/main.py
+++ EM_and_BW_algorithms/main.py
@@ -1,8 +1,3 @@
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-parentdir = os.path.dirname(parentdir)
-sys.path.append(parentdir)
 from examples.examples_models import modelCTMC2, modelCTMC3, modelCTMC_random
 from src.learning.BW_CTMC_Composition import BW_CTMC_Composition
 from src.learning.BW_CTMC import BW_CTMC
@@ -46,6 +41,8 @@ def generateRandomModels(disjoint=False):
 NB_EXPERIMENTS = 40
 v=True
 
+folder = "experiment/CTMC_composition/output/"
+
 dots_compo_compo     = []
 dots_compo_model1    = []
 dots_simple_compo    = []
@@ -59,15 +56,15 @@ original1 = modelCTMC2()
 original2 = modelCTMC3()
 original_model = parallelComposition(original1,original2)
 #timed_training_set, untimed_training_set = generateTrainingSets(1000,10)
-#saveSet(timed_training_set,"output/timed_training_set.txt")
-#saveSet(untimed_training_set,"output/untimed_training_set.txt")
+#saveSet(timed_training_set,folder+"timed_training_set.txt")
+#saveSet(untimed_training_set,folder+"untimed_training_set.txt")
 #timed_test_set_composition, timed_test_set_1 = generateTestSets()
-#saveSet(timed_test_set_composition,"output/timed_test_set_composition.txt")
-#saveSet(timed_test_set_1,"output/timed_test_set_1.txt")
-timed_training_set = loadSet("output/timed_training_set.txt")
-untimed_training_set = loadSet("output/untimed_training_set.txt")
-timed_test_set_composition = loadSet("output/timed_test_set_composition.txt")
-timed_test_set_1 = loadSet("output/timed_test_set_1.txt")
+#saveSet(timed_test_set_composition,folder+"timed_test_set_composition.txt")
+#saveSet(timed_test_set_1,folder+"timed_test_set_1.txt")
+timed_training_set = loadSet(folder+"timed_training_set.txt")
+untimed_training_set = loadSet(folder+"untimed_training_set.txt")
+timed_test_set_composition = loadSet(folder+"timed_test_set_composition.txt")
+timed_test_set_1 = loadSet(folder+"timed_test_set_1.txt")
 quality_original = original_model.logLikelihood(timed_test_set_composition)
 quality_original1= original1.logLikelihood(timed_test_set_1)
 
@@ -75,15 +72,15 @@ original1_disjoint = modelCTMC2('1')
 original2_disjoint = modelCTMC3('2')
 original_model_disjoint = parallelComposition(original1_disjoint,original2_disjoint)
 #timed_training_set_disjoint = generateSet(original_model_disjoint,1000,10,timed=True)
-#saveSet(timed_training_set_disjoint,'output/timed_training_set_disjoint.txt')
+#saveSet(timed_training_set_disjoint,folder+"timed_training_set_disjoint.txt')
 #timed_test_set_composition_disjoint, timed_test_set_1_disjoint, timed_test_set_2_disjoint = generateTestSetsDisjoint()
-#saveSet(timed_test_set_composition_disjoint,"output/timed_test_set_composition_disjoint.txt")
-#saveSet(timed_test_set_1_disjoint,"output/timed_test_set_1_disjoint.txt")
-#saveSet(timed_test_set_2_disjoint,"output/timed_test_set_2_disjoint.txt")
-timed_training_set_disjoint = loadSet("output/timed_training_set_disjoint.txt")
-timed_test_set_composition_disjoint = loadSet("output/timed_test_set_composition_disjoint.txt")
-timed_test_set_1_disjoint = loadSet("output/timed_test_set_1_disjoint.txt")
-timed_test_set_2_disjoint = loadSet("output/timed_test_set_2_disjoint.txt")
+#saveSet(timed_test_set_composition_disjoint,folder+"timed_test_set_composition_disjoint.txt")
+#saveSet(timed_test_set_1_disjoint,folder+"timed_test_set_1_disjoint.txt")
+#saveSet(timed_test_set_2_disjoint,folder+"timed_test_set_2_disjoint.txt")
+timed_training_set_disjoint = loadSet(folder+"timed_training_set_disjoint.txt")
+timed_test_set_composition_disjoint = loadSet(folder+"timed_test_set_composition_disjoint.txt")
+timed_test_set_1_disjoint = loadSet(folder+"timed_test_set_1_disjoint.txt")
+timed_test_set_2_disjoint = loadSet(folder+"timed_test_set_2_disjoint.txt")
 quality_original_disjoint = original_model_disjoint.logLikelihood(timed_test_set_composition_disjoint)
 quality_original1_disjoint = original1_disjoint.logLikelihood(timed_test_set_1_disjoint)
 quality_original2_disjoint = original2_disjoint.logLikelihood(timed_test_set_2_disjoint)
@@ -96,7 +93,7 @@ for exp in range(1,NB_EXPERIMENTS+1):
 
     print("Composition")
     s = datetime.now()
-    timed_model1, timed_model2 = BW_CTMC_Composition(random1,random2).learn(timed_training_set,output_file="output/compo_"+str(exp),verbose=v)
+    timed_model1, timed_model2 = BW_CTMC_Composition(random1,random2).learn(timed_training_set,output_file=folder+"compo_"+str(exp),verbose=v)
     s = (datetime.now()-s).total_seconds()
     timed_model = parallelComposition(timed_model1,timed_model2)
     ll = abs(quality_original-timed_model.logLikelihood(timed_test_set_composition))
@@ -106,14 +103,14 @@ for exp in range(1,NB_EXPERIMENTS+1):
     
     print("Simple")
     s = datetime.now()
-    simple = BW_CTMC(parallelComposition(random1,random2)).learn(timed_training_set,output_file="output/simple_"+str(exp)+".txt",verbose=v)
+    simple = BW_CTMC(parallelComposition(random1,random2)).learn(timed_training_set,output_file=folder+"simple_"+str(exp)+".txt",verbose=v)
     s = (datetime.now()-s).total_seconds()
     ll = abs(quality_original-simple.logLikelihood(timed_test_set_composition))
     dots_simple_compo.append((s,ll))
     
     print("Model1")
     s = datetime.now()
-    untimed_model1, _ = BW_CTMC_Composition(random1,original2).learn(untimed_training_set,output_file="output/model1_"+str(exp),to_update=1,verbose=v)
+    untimed_model1, _ = BW_CTMC_Composition(random1,original2).learn(untimed_training_set,output_file=folder+"model1_"+str(exp),to_update=1,verbose=v)
     s = (datetime.now()-s).total_seconds()
     ll = abs(quality_original-parallelComposition(untimed_model1,_).logLikelihood(timed_test_set_composition))
     dots_model1_compo.append((s,ll))
@@ -137,7 +134,7 @@ for exp in range(1,NB_EXPERIMENTS+1):
     duration_tot += datetime.now()-start_exp
     print("Expected ETA:",datetime.now()+(NB_EXPERIMENTS-exp)*duration_tot/exp)
 
-f = open("output/summary.txt",'w')
+f = open(folder+"summary.txt",'w')
 f.write(str(dots_compo_compo)    +'\n')
 f.write(str(dots_compo_model1)   +'\n')
 f.write(str(dots_simple_compo)   +'\n')
@@ -148,8 +145,7 @@ f.write(str(dots_model1_disjoint)+'\n')
 f.write(str(dots_model2_disjoint)+'\n')
 f.close()
 
-"""
-f = open("output/summary.txt",'r')
+f = open(folder+"summary.txt",'r')
 dots_compo_compo     = literal_eval(f.readline()[:-1])
 dots_compo_model1    = literal_eval(f.readline()[:-1])
 dots_simple_compo    = literal_eval(f.readline()[:-1])
@@ -165,13 +161,12 @@ ax.scatter([i[0] for i in dots_simple_compo], [i[1] for i in dots_simple_compo],
 ax.scatter([i[0] for i in dots_compo_compo],  [i[1] for i in dots_compo_compo],  c='r', alpha=0.5, label="Exp. 2: learning U and V")
 ax.scatter([i[0] for i in dots_model1_compo], [i[1] for i in dots_model1_compo], c='b', alpha=0.5, label="Exp. 3: learning V")
 ax.set_xlabel("Running time (s)")
-ax.set_ylabel("Quality of the composition")
+ax.set_ylabel("Distance")
 ax.legend()
 plt.show()
 
 fig, ax = plt.subplots(figsize=(5,5))
 ax.boxplot([[i[1] for i in dots_model_disjoint],[i[1] for i in dots_model1_disjoint],[i[1] for i in dots_model2_disjoint]])
-ax.set_xticks([1,2,3], ["U||V","U","V"])
+ax.set_xticklabels([1,2,3], ["U||V","U","V"])
 ax.set_ylabel("Model quality")
 plt.show()
-"""
