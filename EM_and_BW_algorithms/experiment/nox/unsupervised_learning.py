b72538efaa495b3a5e56786888a524d40e0e7ddb
kraken_laptop <raphal20@ru.is>
2022-05-10 15:25:10 +0200

    nox with levinson and GOHMM

--- EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
+++ EM_and_BW_algorithms/experiment/nox/unsupervised_learning.py
@@ -5,23 +5,21 @@ parentdir = os.path.dirname(parentdir)
 sys.path.append(parentdir)
 
 from experiment.nox.edfreader import EDFreader
-from src.tools import saveSet, loadSet, setFromList, getAlphabetFromSequences
-from src.learning.BW_HMM import BW_HMM
+from src.tools import saveSet
+from src.learning.BW_GOHMM import BW_GOHMM
 from src.learning.BW import BW
-from src.models.HMM import loadHMM, HMM
-from examples.examples_models import modelHMM_random
+from src.models.GOHMM import GOHMM
+from examples.examples_models import modelGOHMM_nox
 import numpy as np
-from pyts.approximation import SymbolicFourierApproximation
-from random import shuffle
 import pandas as pd
 from datetime import datetime
 from statistics import mean
-from itertools import product
-
+from levinson import levinson
 MANUAL_SCORING_WINDOW_SEC = 30
 
-WINDOW_SIZE_SEC = 10 #nb of sec as input to DFA
-NB_WINDOWS_BY_SEQ = 2*6 #nb of sec by sequence = WINDOW_SIZE_SEC_MAX*NB_WINDOWS_BY_SEQ
+# OFFSET_SEC = 1
+WINDOW_SIZE_SEC = 1 #nb of sec as input to DFA
+NB_WINDOWS_BY_SEQ = 10//WINDOW_SIZE_SEC #nb of sec by sequence = WINDOW_SIZE_SEC*NB_WINDOWS_BY_SEQ
 
 NB_STATES = 5
 
@@ -58,8 +56,8 @@ def read_files(psg_number: int, signal_id: int):
 	edf_file , hypno_file = file_paths_from_psg_number(psg_number)
 	
 	r = EDFreader(edf_file)
-	length = r.getTotalSamples(signal_id)
 	frequency = r.getSampleFrequency(signal_id)
+	print(frequency)
 
 	begining, duration = find_starting_ending_point(r,frequency,hypno_file)
 	
@@ -75,29 +73,27 @@ def read_files(psg_number: int, signal_id: int):
 	return data
 
 
-def write_set(psg_numbers: list,signal_id,name,n_coefs=4,n_bins=6):
+def write_set(psg_numbers: list,signal_id,name):
 	"""name is the name of the output files,
-	fraction_test is a float between ]0,1[ corresponding to the fraction of sequences in the test set """
+	fraction_test is a float between ]0,1[ corresponding to the fraction of
+	sequences in the test set """
 	new_data = []
-	transformer = SymbolicFourierApproximation(n_coefs=n_coefs,n_bins=n_bins)
 	for psg_number in psg_numbers:
 		print("PSG:",psg_number, "Signal:",signal_id)
 		data = read_files(psg_number,signal_id)
 		try:
-			data = transformer.fit_transform(data)
-			data = [''.join(i) for i in data]
+			data = [levinson(i)[2][0] for i in data]
 			for i in range(0,len(data) - NB_WINDOWS_BY_SEQ,NB_WINDOWS_BY_SEQ):
 				new_data.append([data[i+j] for j in range(NB_WINDOWS_BY_SEQ)])
 			new_data.append([data[i+j] for j in range(len(data)%NB_WINDOWS_BY_SEQ)])
 		except ValueError:
 			print("ERROR with",psg_number,"-",signal_id)
 	data = new_data
-	data = setFromList(data)
 	if name != False:
 		saveSet(data, name+".txt")
 	return data
 
-def evaluation(m: HMM, signal_id, psg_numbers: list) -> list:
+def evaluation(m: GOHMM, signal_id, psg_numbers: list) -> list:
 	sleep_stages = ["Wake","N1","N2","N3","REM"]
 	bw = BW(m)
 	corr_matrix = []
@@ -127,32 +123,22 @@ def evaluation(m: HMM, signal_id, psg_numbers: list) -> list:
 
 
 
-n_bins  = 5 # nb of letters
-# alphabet = list("abcdefghijklmnopqrstuvwxyz")[:n_bins]
-n_coefs = 4 # 5 because delta, theta, alpha, beta1, beta2 activity
-
-list_signals = [     20] #,     24,     30,    34,      44,     48,     67,     71] 
-signals_name = ["C3-M2"] #,"C4-M1","E1-M2","E2-M1","F3-M2","F4-M1","O1-M2","O2-M1"]
-
-# size alphabet = n_bins**n_coefs
-# nb possible sequences = (size alphabet)**NB_WINDOWS_BY_SEQ 
+signals_ids  = [     20] #,     24,     30,    34,      44,     48,     67,     71] 
+signals_names = ["C3-M2"] #,"C4-M1","E1-M2","E2-M1","F3-M2","F4-M1","O1-M2","O2-M1"]
 psgs = list(range(1,51))
 #shuffle(psgs)
-training_psgs = psgs[:25]
-test_psgs = psgs[25:30]
-alphabet = [''.join(j) for j in list(product(*[[chr(i) for i in range(97,97+n_bins)]]*n_coefs))]
+training_psgs = [psgs[0]]
+#test_psgs = psgs[25:30]
 running_times = []
 
-for signal_index in range(len(list_signals)):
-	signal_id = list_signals[signal_index]
-	signal_name = signals_name[signal_index]
-	
-	tr = write_set(training_psgs,signal_id,"training_set",n_coefs,n_bins)
-	rm = modelHMM_random(NB_STATES,alphabet,random_initial_state=True)
-	algo = BW_HMM(rm)
+for signal_id, signal_name in zip(signals_ids, signals_names):
+	tr = write_set(training_psgs,signal_id,"training_set")
+	rm = modelGOHMM_nox(NB_STATES,random_initial_state=True)
+	algo = BW_GOHMM(rm)
 	starting_time = datetime.now()
 	out = algo.learn(tr, output_file="model_"+signal_name+".txt", verbose=True)
 	running_times.append((datetime.now()-starting_time).total_seconds())
+	"""
 	corr_matrix = evaluation(out, signal_id, test_psgs)
 	string  = signal_name+'\n'
 	string += " "*8+'|  Wake  |   N1   |   N2   |   N3   |  REM   '
@@ -168,4 +154,6 @@ for signal_index in range(len(list_signals)):
 	f.write(string)
 	f.close()
 	print(string)
+	"""
+out.pprint()
 print("Average learning time",mean(running_times))
